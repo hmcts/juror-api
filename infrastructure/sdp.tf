e3d9928c06072d73d8fc98575bea43509d68ffd9
Ian Fisher - CGI <142427330+Ianrfisher@users.noreply.github.com>
2024-01-29 17:25:56 +0000

    add db user for sdp (#16)
    * add db user for sdp
    
    * add provider
    
    * add provider to sdp user block
    
    * add env
    
    * try aks id instead
    
    * set server-name to juror
    
    * alter db name and sdp naming
    
    * put names back
    
    * Update infrastructure/sdp.tf
    
    Co-authored-by: Neil Perry <142518199+npv0@users.noreply.github.com>
    
    * use include list instead of exclude
    
    ---------
    
    Co-authored-by: Neil Perry <142518199+npv0@users.noreply.github.com>

---
+++ infrastructure/sdp.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "sdp_vault"
+  subscription_id            = var.aks_subscription_id
+}
+
+module "sdp_db_user" {
+
+  providers = {
+    azurerm.sdp_vault = azurerm.sdp_vault
+  }
+
+  source = "git@github.com:hmcts/terraform-module-sdp-db-user?ref=master"
+
+  env               = var.env
+  server_name       = "${var.product}-${var.component}-flexible-${var.env}"
+  server_fqdn       = module.postgresql_flexible.fqdn
+  server_admin_user = module.postgresql_flexible.username
+  server_admin_pass = module.postgresql_flexible.password
+
+  count = var.env == "prod" || var.env == "stg" || var.env == "ithc" ? 1 : 0
+
+  databases = [
+    {
+      name : "juror"
+    }
+  ]
+
+  database_schemas = {
+    juror = ["juror_mod"]
+  }
+
+  common_tags = var.common_tags
+
+  depends_on = [
+    module.postgresql_flexible
+  ]
+}
