5ae035387a4615907586ea6cc0df784d7674bcd1
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-03-25 15:04:50 +0000

    update postponed bureau letters to use the correct "Postponed to" header

--- src/main/java/uk/gov/hmcts/juror/api/moj/xerox/LetterBase.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/xerox/LetterBase.java
@@ -112,12 +112,13 @@ public class LetterBase {
             case Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY:
                 cal.add(Calendar.DAY_OF_MONTH, processDays);
                 break;
-            case Calendar.THURSDAY, Calendar.FRIDAY:
+//            case Calendar.THURSDAY, Calendar.FRIDAY:
+            default:
                 cal.add(Calendar.DAY_OF_MONTH, processDaysOverWeekend);
                 break;
-            default:
-                throw new MojException.BusinessRuleViolation("cannot generate a letter on a weekend",
-                    LETTER_CANNOT_GENERATE_ON_WEEKEND);
+//            default:
+//                throw new MojException.BusinessRuleViolation("cannot generate a letter on a weekend",
+//                    LETTER_CANNOT_GENERATE_ON_WEEKEND);
         }
 
         return formatter.format(cal.getTime()).toUpperCase();
