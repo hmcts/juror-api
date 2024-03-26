3a6db0ddab06618398e8d5ee556220255055d6b3
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2024-03-26 08:54:56 +0000

    hotfix/JM-6578 (#120)

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/summonsmanagement/JurorResponseRetrieveResponseDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/summonsmanagement/JurorResponseRetrieveResponseDto.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.juror.api.moj.controller.response.summonsmanagement;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -70,6 +71,7 @@ public class JurorResponseRetrieveResponseDto {
 
         @JsonProperty("date_received")
         @Schema(description = "Date response received")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         private LocalDateTime dateReceived;
 
         @JsonProperty("reply_type")
