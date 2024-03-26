8ec41629aabf9031710cdc1e5f6e6adb2df5a632
Craig Chapman <craig.chapman@cgi.com>
2024-03-26 15:47:16 +0000

    Create V1_42__create_dashboard_stats_tables.sql
    - copy table definitions from juror_digital schema to new juror_dashboard schema

---
+++ src/main/resources/db/migration/V1_42__create_dashboard_stats_tables.sql
@@ -0,0 +1,72 @@
+create schema if not exists juror_dashboard;
+
+create table juror_dashboard.stats_auto_processed (
+	processed_date timestamp(0) not null,
+	processed_count int null default 0,
+	constraint stats_auto_processed_pkey primary key (processed_date)
+);
+
+create table juror_dashboard.stats_deferrals (
+	bureau_or_court varchar(6) not null,
+	exec_code varchar(1) not null,
+	calendar_year varchar(4) not null,
+	financial_year varchar(7) not null,
+	week varchar(7) not null,
+	excusal_count int not null,
+	constraint stats_deferrals_pkey primary key (bureau_or_court, exec_code, calendar_year, financial_year, week)
+);
+
+create table juror_dashboard.stats_excusals (
+	bureau_or_court varchar(6) not null,
+	exec_code varchar(1) not null,
+	calendar_year varchar(4) not null,
+	financial_year varchar(7) not null,
+	week varchar(7) not null,
+	excusal_count int not null,
+	constraint stats_excusals_pkey primary key (bureau_or_court, exec_code, calendar_year, financial_year, week)
+);
+
+create table juror_dashboard.stats_not_responded (
+	summons_month timestamp(0) not null,
+	loc_code varchar(3) not null,
+	non_responsed_count int null default 0,
+	constraint stats_not_responded_pkey primary key (summons_month, loc_code)
+);
+
+create table juror_dashboard.stats_response_times (
+	summons_month timestamp(0) not null,
+	response_month timestamp(0) not null,
+	response_period varchar(15) not null,
+	loc_code varchar(3) not null,
+	response_method varchar(13) not null,
+	response_count int null default 0,
+	constraint stats_response_times_pkey primary key (summons_month, response_month, response_period, loc_code, response_method)
+);
+
+create table juror_dashboard.stats_thirdparty_online (
+	summons_month timestamp(0) not null,
+	thirdparty_response_count int null default 0,
+	constraint stats_thirdparty_online_pkey primary key (summons_month)
+);
+
+create table juror_dashboard.stats_unprocessed_responses (
+	loc_code varchar(3) not null,
+	unprocessed_count int null default 0,
+	constraint stats_unprocessed_responses_pkey primary key (loc_code)
+);
+
+create table juror_dashboard.stats_welsh_online_responses (
+	summons_month timestamp(0) not null,
+	welsh_response_count int null default 0,
+	constraint stats_welsh_online_responses_pkey primary key (summons_month)
+);
+
+create table juror_dashboard.survey_response (
+	id varchar(20) not null,
+	survey_id varchar(20) not null,
+	user_no int null,
+	survey_response_date timestamp(0) null,
+	satisfaction_desc varchar(50) null,
+	created timestamp null default (current_timestamp at time zone 'utc'::text),
+	constraint survey_response_pkey primary key (id, survey_id)
+);
