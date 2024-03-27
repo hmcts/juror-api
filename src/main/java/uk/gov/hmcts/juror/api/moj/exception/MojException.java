f5bf8c5cb2badf85509397eb1694b5b8104788ea
benedwards <ben.edwards@cgi.com>
2024-03-27 09:55:59 +0000

    Fixed styles

--- src/main/java/uk/gov/hmcts/juror/api/moj/exception/MojException.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/exception/MojException.java
@@ -39,11 +39,13 @@ public class MojException extends RuntimeException {
         public BusinessRuleViolation(String customErrorMessage, ErrorCode errorCode) {
             this(customErrorMessage, errorCode, null);
         }
-        public BusinessRuleViolation(String customErrorMessage, ErrorCode errorCode, Map<String,Object> metaData) {
+
+        public BusinessRuleViolation(String customErrorMessage, ErrorCode errorCode, Map<String, Object> metaData) {
             this(customErrorMessage, errorCode, metaData, null);
         }
 
-        public BusinessRuleViolation(String customErrorMessage, ErrorCode errorCode, Map<String,Object> metaData, Throwable cause) {
+        public BusinessRuleViolation(String customErrorMessage, ErrorCode errorCode, Map<String, Object> metaData,
+                                     Throwable cause) {
             super(customErrorMessage, cause);
             this.errorCode = errorCode;
             this.metaData = metaData;
