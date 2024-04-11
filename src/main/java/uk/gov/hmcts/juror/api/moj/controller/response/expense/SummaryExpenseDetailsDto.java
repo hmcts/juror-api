c323d615b74521eb5bd64b00940cf8700afa1a0a
akikrahman1 <akhlaqur.rahman1@hmcts.net>
2024-04-11 17:51:29 +0100

    Update SummaryExpenseDetailsDto.java

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/expense/SummaryExpenseDetailsDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/expense/SummaryExpenseDetailsDto.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
