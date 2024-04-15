cc3d35886923a94a2ef5bda9f3e33a0e1d46a755
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-04-15 16:30:11 +0100

    Code Optimisations for Your work pages (#198)
    * Added voters optimizations
    
    * Optimised getTodo
    
    * Optimized juror response views
    
    * Added Juror Response indexs

---
+++ src/main/resources/db/migration/V1_55__Performance_Updates.sql
@@ -0,0 +1,33 @@
+alter table juror_mod.voters
+    add postcode_start VARCHAR(4);
+
+UPDATE juror_mod.voters
+SET postcode_start = split_part(zip, ' ', 1);
+
+-- DROP FUNCTION juror_mod.get_voters(int8, text, text, text, text, text);
+
+CREATE OR REPLACE FUNCTION juror_mod.get_voters(p_required bigint, p_mindate date, p_maxdate date, p_loccode text,
+                                                p_areacode_list text, p_pool_type text)
+    RETURNS TABLE(part_number character varying, juror_flags character varying)
+    LANGUAGE plpgsql
+    STABLE SECURITY DEFINER
+AS $function$
+DECLARE
+begin
+    return query select part_no, flags from juror_mod.voters
+                 where loc_code=p_LocCode and date_selected1 is null
+                   and ((dob is null) or dob > p_minDate and dob < p_maxDate)
+                   and perm_disqual is null
+                   and (postcode_start in (select unnest(string_to_array(p_areacode_list, ',')))) -- specified postcode areas
+                   and (flags is null or p_pool_type = 'N') -- only coroner pools check flag
+                 order by random()
+                 limit p_required*1.2; -- grab 20% more than requested to allow for jurors with flags
+
+END;
+$function$
+;
+
+CREATE INDEX juror_response_processing_status_idx ON juror_mod.juror_response USING btree (processing_status);
+CREATE INDEX juror_response_staff_login_idx ON juror_mod.juror_response USING btree (staff_login);
+CREATE INDEX juror_response_staff_login_processing_status_idx ON juror_mod.juror_response USING btree (staff_login, processing_status);
+CREATE INDEX juror_history_history_code_idx ON juror_mod.juror_history USING btree (history_code);
