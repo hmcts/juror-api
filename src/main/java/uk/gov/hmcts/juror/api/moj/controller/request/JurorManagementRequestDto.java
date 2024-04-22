94be69fd29632b5e2fd6d2e511eb193ca36a80b6
Greg Skinner <greg.skinner@cgi.com>
2024-04-22 15:05:34 +0100

    Chore (JM-7046) add check exception for validating postponement from deferral maintenance

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/JurorManagementRequestDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/JurorManagementRequestDto.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.juror.api.moj.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import uk.gov.hmcts.juror.api.validation.NumericString;
 
 import java.time.LocalDate;
@@ -65,6 +68,10 @@ public class JurorManagementRequestDto {
     @Size(min = 3, max = 3)
     @NumericString
     private String sendingCourtLocCode;
+
+    @JsonProperty("deferralMaintenance")
+    @Schema(description = "Deferral is occurring from deferral maintenance")
+    public Boolean deferralMaintenance;
 
     //constructor to handle transfer jurors requests
     public JurorManagementRequestDto(String sourcePoolNumber, String sendingCourtLocCode, String receivingCourtLocCode,
