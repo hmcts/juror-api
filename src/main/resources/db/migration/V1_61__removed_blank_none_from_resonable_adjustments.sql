85a13f1ad4a162b7cf8f7d5778fe98cce7e4b3a8
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-04-25 14:51:44 +0100

    Updated migration to update juror reasonable adjustment code before deleting the redundant code (#257)
    * Updated migration to update juror code before deleting the code
    
    * Updated migration to update juror code before deleting the code

--- src/main/resources/db/migration/V1_61__removed_blank_none_from_resonable_adjustments.sql
+++ src/main/resources/db/migration/V1_61__removed_blank_none_from_resonable_adjustments.sql
@@ -1,2 +1,11 @@
-DELETE FROM juror_mod.t_reasonable_adjustments
+update juror_mod.juror
+set reasonable_adj_code = 'N'
+where reasonable_adj_code = ' ';
+
+update juror_mod.juror_reasonable_adjustment
+set reasonable_adjustment = 'N'
+where reasonable_adjustment = ' ';
+
+DELETE
+FROM juror_mod.t_reasonable_adjustments
 where code = ' ';
