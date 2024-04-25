32e0850f02c48e0cce777706c74ea896576c40d4
benedwards <ben.edwards@cgi.com>
2024-04-25 14:33:05 +0100

    Updated migration to update juror code before deleting the code

--- src/main/resources/db/migration/V1_61__removed_blank_none_from_resonable_adjustments.sql
+++ src/main/resources/db/migration/V1_61__removed_blank_none_from_resonable_adjustments.sql
@@ -1,2 +1,7 @@
-DELETE FROM juror_mod.t_reasonable_adjustments
+update juror_mod.juror
+set reasonable_adj_code = 'N'
+where reasonable_adj_code = ' ';
+
+DELETE
+FROM juror_mod.t_reasonable_adjustments
 where code = ' ';
