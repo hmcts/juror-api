82171c409f59f10e410ae6ade094a01ecf88cee9
benedwards <ben.edwards@cgi.com>
2024-04-30 13:22:17 +0100

    Fixed integration tests

--- src/integration-test/java/uk/gov/hmcts/juror/api/moj/controller/AuthenticationControllerITest.java
+++ src/integration-test/java/uk/gov/hmcts/juror/api/moj/controller/AuthenticationControllerITest.java
@@ -226,6 +226,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
 
                 assertThat(claims)
                     .containsEntry("owner", expectedJwtClaims.getOwner())
+                    .containsEntry("email", expectedJwtClaims.getEmail())
                     .containsEntry("locCode", expectedJwtClaims.getLocCode())
                     .containsEntry("login", expectedJwtClaims.getLogin())
                     .containsEntry("userLevel", expectedJwtClaims.getUserLevel())
@@ -239,7 +240,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         "courts", expectedJwtClaims.getStaff().getCourts()
                     ));
 
-                assertThat(claims).hasSize(13)
+                assertThat(claims).hasSize(14)
                     .containsEntry("roles", expectedJwtClaims.getRoles()
                         .stream().map(Enum::name).toList());
             }
@@ -255,6 +256,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         "test_court_primary",
                         BureauJwtPayload.builder()
                             .owner("408")
+                            .email("test_court_primary" + EMAIL_SUFFIX)
                             .locCode("408")
                             .login("test_court_primary")
                             .userLevel("1")
@@ -282,6 +284,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         BureauJwtPayload.builder()
                             .owner("415")
                             .locCode("462")
+                            .email("test_court_standard" + EMAIL_SUFFIX)
                             .login("test_court_standard")
                             .userLevel("1")
                             .userType(UserType.COURT)
@@ -308,6 +311,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         BureauJwtPayload.builder()
                             .owner("415")
                             .locCode("767")
+                            .email("test_admin_standard" + EMAIL_SUFFIX)
                             .login("test_admin_standard")
                             .userLevel("0")
                             .userType(UserType.ADMINISTRATOR)
@@ -332,6 +336,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         "test_bureau_standard",
                         BureauJwtPayload.builder()
                             .owner("400")
+                            .email("test_bureau_standard" + EMAIL_SUFFIX)
                             .locCode("400")
                             .login("test_bureau_standard")
                             .userLevel("0")
@@ -358,6 +363,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         BureauJwtPayload.builder()
                             .owner("400")
                             .locCode("400")
+                            .email("test_admin_standard" + EMAIL_SUFFIX)
                             .login("test_admin_standard")
                             .userLevel("0")
                             .userType(UserType.ADMINISTRATOR)
@@ -383,6 +389,7 @@ public class AuthenticationControllerITest extends AbstractIntegrationTest {
                         BureauJwtPayload.builder()
                             .owner("415")
                             .locCode("415")
+                            .email("test_court_sjo_mangr" + EMAIL_SUFFIX)
                             .login("test_court_sjo_mangr")
                             .userLevel("9")
                             .userType(UserType.COURT)
