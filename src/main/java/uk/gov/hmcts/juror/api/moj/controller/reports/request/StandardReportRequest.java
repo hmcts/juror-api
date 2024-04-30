986e32e3475345065a3b155e094be867edacf4a8
zacmnolan <Zac.Nolan@HMCTS.NET>
2024-04-30 14:53:41 +0100

    Update StandardReportRequest.java

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/reports/request/StandardReportRequest.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/reports/request/StandardReportRequest.java
@@ -37,7 +37,7 @@ public class StandardReportRequest {
         "UndeliverableListReport",
         "PanelSummaryReport",
         "IncompleteServiceReport",
-        "PanelListDetailReport",
+        "PanelListDetailedReport",
         //Grouped
         "PostponedListByDateReport"
     })
