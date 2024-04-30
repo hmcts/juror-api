0227b26520726bfdf5058734901243f6a6c2a156
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2024-04-30 16:08:47 +0100

    hotfix/JM-6713 (#275)
    * hotfix/JM-6713
    
    * Update DeceasedResponseServiceTest.java
    
    * Update DeceasedResponseServiceTest.java
    
    * updates following review
    
    * fix PMD issues

--- src/test/java/uk/gov/hmcts/juror/api/moj/service/DeceasedResponseServiceTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/service/DeceasedResponseServiceTest.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.juror.api.moj.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.juror.api.config.bureau.BureauJwtPayload;
 import uk.gov.hmcts.juror.api.juror.domain.CourtLocation;
+import uk.gov.hmcts.juror.api.juror.domain.ProcessingStatus;
 import uk.gov.hmcts.juror.api.moj.controller.request.MarkAsDeceasedDto;
 import uk.gov.hmcts.juror.api.moj.domain.ContactCode;
 import uk.gov.hmcts.juror.api.moj.domain.ContactEnquiryCode;
@@ -17,6 +17,8 @@ import uk.gov.hmcts.juror.api.moj.domain.Juror;
 import uk.gov.hmcts.juror.api.moj.domain.JurorPool;
 import uk.gov.hmcts.juror.api.moj.domain.JurorStatus;
 import uk.gov.hmcts.juror.api.moj.domain.PoolRequest;
+import uk.gov.hmcts.juror.api.moj.domain.jurorresponse.DigitalResponse;
+import uk.gov.hmcts.juror.api.moj.domain.jurorresponse.PaperResponse;
 import uk.gov.hmcts.juror.api.moj.exception.MojException;
 import uk.gov.hmcts.juror.api.moj.repository.ContactCodeRepository;
 import uk.gov.hmcts.juror.api.moj.repository.ContactEnquiryTypeRepository;
@@ -24,17 +26,31 @@ import uk.gov.hmcts.juror.api.moj.repository.ContactLogRepository;
 import uk.gov.hmcts.juror.api.moj.repository.JurorHistoryRepository;
 import uk.gov.hmcts.juror.api.moj.repository.JurorPoolRepository;
 import uk.gov.hmcts.juror.api.moj.repository.JurorRepository;
+import uk.gov.hmcts.juror.api.moj.repository.jurorresponse.JurorDigitalResponseRepositoryMod;
 import uk.gov.hmcts.juror.api.moj.repository.jurorresponse.JurorPaperResponseRepositoryMod;
+import uk.gov.hmcts.juror.api.moj.repository.jurorresponse.JurorResponseCommonRepositoryMod;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-public class DeceasedResponseServiceTest {
+@SuppressWarnings("PMD.LawOfDemeter")
+class DeceasedResponseServiceTest {
     @Mock
     private JurorHistoryRepository jurorHistoryRepository;
     @Mock
@@ -44,7 +60,11 @@ public class DeceasedResponseServiceTest {
     @Mock
     private JurorPoolRepository jurorPoolRepository;
     @Mock
+    private JurorResponseCommonRepositoryMod jurorResponseCommonRepositoryMod;
+    @Mock
     private JurorPaperResponseRepositoryMod jurorPaperResponseRepository;
+    @Mock
+    private JurorDigitalResponseRepositoryMod jurorDigitalResponseRepository;
     @Mock
     private ContactEnquiryTypeRepository contactEnquiryTypeRepository;
     @Mock
@@ -52,13 +72,36 @@ public class DeceasedResponseServiceTest {
 
     public static final String UNABLE_TO_ATTEND = "UA";
 
-
     @InjectMocks
     private DeceasedResponseServiceImpl deceasedResponseService;
 
 
+    @BeforeEach
+    void beforeEach() {
+        jurorHistoryRepository = mock(JurorHistoryRepository.class);
+        contactCodeRepository = mock(ContactCodeRepository.class);
+        jurorRepository = mock(JurorRepository.class);
+        jurorPoolRepository = mock(JurorPoolRepository.class);
+        jurorResponseCommonRepositoryMod = mock(JurorResponseCommonRepositoryMod.class);
+        jurorPaperResponseRepository = mock(JurorPaperResponseRepositoryMod.class);
+        jurorDigitalResponseRepository = mock(JurorDigitalResponseRepositoryMod.class);
+        contactEnquiryTypeRepository = mock(ContactEnquiryTypeRepository.class);
+        contactLogRepository = mock(ContactLogRepository.class);
+
+        this.deceasedResponseService = new DeceasedResponseServiceImpl(
+            contactCodeRepository,
+            jurorHistoryRepository,
+            jurorPoolRepository,
+            jurorRepository,
+            jurorPaperResponseRepository,
+            jurorDigitalResponseRepository,
+            jurorResponseCommonRepositoryMod,
+            contactLogRepository
+        );
+    }
+
     @Test
-    public void test_jurorRecordDoesNotExist() {
+    void jurorRecordDoesNotExist() {
         final String owner = "400";
         String jurorNumber = "123456789";
 
@@ -70,18 +113,18 @@ public class DeceasedResponseServiceTest {
 
         List<JurorPool> jurorPools = new ArrayList<>();
 
-        Mockito.doReturn(jurorPools).when(jurorPoolRepository)
+        doReturn(jurorPools).when(jurorPoolRepository)
             .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(jurorNumber, true);
         assertThatExceptionOfType(MojException.NotFound.class).isThrownBy(
             () -> deceasedResponseService.markAsDeceased(buildPayload(owner), markAsDeceasedDto));
 
-        Mockito.verify(jurorPoolRepository, Mockito.times(1))
+        verify(jurorPoolRepository, times(1))
             .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(jurorNumber, true);
-        Mockito.verify(jurorPoolRepository, Mockito.never()).save(Mockito.any());
+        verify(jurorPoolRepository, never()).save(any());
     }
 
     @Test
-    public void test_jurorRecordIsNotOwned() {
+    void jurorRecordIsNotOwned() {
         final String owner = "415";
         String jurorNumber = "123456789";
 
@@ -94,19 +137,19 @@ public class DeceasedResponseServiceTest {
         List<JurorPool> jurorPools = new ArrayList<>();
         jurorPools.add(createValidJurorPool(jurorNumber, "400"));
 
-        Mockito.doReturn(jurorPools).when(jurorPoolRepository)
+        doReturn(jurorPools).when(jurorPoolRepository)
             .findByJurorJurorNumberAndIsActive(jurorNumber, true);
 
         assertThatExceptionOfType(MojException.Forbidden.class).isThrownBy(
             () -> deceasedResponseService.markAsDeceased(buildPayload(owner), markAsDeceasedDto));
 
-        Mockito.verify(jurorPoolRepository, Mockito.times(1))
+        verify(jurorPoolRepository, times(1))
             .findByJurorJurorNumberAndIsActive(jurorNumber, true);
-        Mockito.verify(jurorPoolRepository, Mockito.never()).save(Mockito.any());
+        verify(jurorPoolRepository, never()).save(any());
     }
 
     @Test
-    public void test_markJurorAsDeceased_paperResponseExists() {
+    void markJurorAsDeceasedPaperResponseExistsFlagTrue() {
         String owner = "400";
         String jurorNumber = "222222225";
 
@@ -122,7 +165,7 @@ public class DeceasedResponseServiceTest {
 
         jurorPools.add(jurorPool);
 
-        Mockito.doReturn(jurorPools).when(jurorPoolRepository)
+        doReturn(jurorPools).when(jurorPoolRepository)
             .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(jurorNumber, true);
 
         ContactEnquiryType enquiryType = new ContactEnquiryType(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND),
@@ -130,27 +173,27 @@ public class DeceasedResponseServiceTest {
 
         Optional<ContactEnquiryType> enquiryTypeOpt = Optional.of(enquiryType);
 
-        Mockito.doReturn(enquiryTypeOpt).when(contactEnquiryTypeRepository)
+        doReturn(enquiryTypeOpt).when(contactEnquiryTypeRepository)
             .findById(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND));
 
         ContactCode contactCode = new ContactCode(
             IContactCode.UNABLE_TO_ATTEND.getCode(),
             IContactCode.UNABLE_TO_ATTEND.getDescription());
-        Mockito.when(contactCodeRepository.findById(
+        when(contactCodeRepository.findById(
             IContactCode.UNABLE_TO_ATTEND.getCode())).thenReturn(Optional.of(contactCode));
 
         deceasedResponseService.markAsDeceased(buildPayload(owner), markAsDeceasedDto);
 
-        Mockito.verify(jurorPoolRepository, Mockito.times(1))
-            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(Mockito.any(), Mockito.anyBoolean());
-        Mockito.verify(jurorPoolRepository, Mockito.times(1)).save(Mockito.any());
-        Mockito.verify(contactCodeRepository, Mockito.times(1)).findById(Mockito.any());
-        Mockito.verify(jurorPaperResponseRepository, Mockito.times(1)).save(Mockito.any());
-        Mockito.verify(contactLogRepository, Mockito.times(1)).saveAndFlush(Mockito.any());
+        verify(jurorPoolRepository, times(1))
+            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(any(), anyBoolean());
+        verify(jurorPoolRepository, times(1)).save(any());
+        verify(contactCodeRepository, times(1)).findById(any());
+        verify(jurorPaperResponseRepository, times(1)).save(any());
+        verify(contactLogRepository, times(1)).saveAndFlush(any());
     }
 
     @Test
-    public void test_markJurorAsDeceased_paperResponseDoesNotExists() {
+    void markJurorAsDeceasedPaperResponseExists() {
         String owner = "400";
         String jurorNumber = "222222225";
 
@@ -158,15 +201,13 @@ public class DeceasedResponseServiceTest {
 
         markAsDeceasedDto.setJurorNumber(jurorNumber);
         markAsDeceasedDto.setDeceasedComment("Brother phoned in to say juror is deceased.");
-        markAsDeceasedDto.setPaperResponseExists(false);
+        markAsDeceasedDto.setPaperResponseExists(false); // this is the default value
 
         JurorPool jurorPool = createValidJurorPool(jurorNumber, owner);
-
         List<JurorPool> jurorPools = new ArrayList<>();
-
         jurorPools.add(jurorPool);
 
-        Mockito.doReturn(jurorPools).when(jurorPoolRepository)
+        doReturn(jurorPools).when(jurorPoolRepository)
             .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(jurorNumber, true);
 
         ContactEnquiryType enquiryType = new ContactEnquiryType(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND),
@@ -174,26 +215,139 @@ public class DeceasedResponseServiceTest {
 
         Optional<ContactEnquiryType> enquiryTypeOpt = Optional.of(enquiryType);
 
-        Mockito.doReturn(enquiryTypeOpt).when(contactEnquiryTypeRepository)
+        doReturn(enquiryTypeOpt).when(contactEnquiryTypeRepository)
             .findById(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND));
 
         ContactCode contactCode = new ContactCode(
             IContactCode.UNABLE_TO_ATTEND.getCode(),
             IContactCode.UNABLE_TO_ATTEND.getDescription());
-        Mockito.when(contactCodeRepository.findById(
+        when(contactCodeRepository.findById(
             IContactCode.UNABLE_TO_ATTEND.getCode())).thenReturn(Optional.of(contactCode));
+
+        doReturn(null).when(jurorDigitalResponseRepository)
+            .findByJurorNumber(jurorNumber);
+
+        PaperResponse jurorResponse = new PaperResponse();
+        jurorResponse.setJurorNumber(jurorNumber);
+
+        doReturn(jurorResponse).when(jurorResponseCommonRepositoryMod)
+            .findByJurorNumber(jurorNumber);
+
+        doReturn(null).when(jurorPaperResponseRepository)
+            .save(any(PaperResponse.class));
 
         deceasedResponseService.markAsDeceased(buildPayload(owner), markAsDeceasedDto);
 
-        Mockito.verify(jurorPoolRepository, Mockito.times(1))
-            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(Mockito.any(), Mockito.anyBoolean());
-        Mockito.verify(jurorPoolRepository, Mockito.times(1)).save(Mockito.any());
-        Mockito.verify(contactCodeRepository, Mockito.times(1)).findById(Mockito.any());
-        Mockito.verify(contactLogRepository, Mockito.times(1)).saveAndFlush(Mockito.any());
+        verify(jurorPoolRepository, times(1))
+            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(any(), anyBoolean());
+        verify(jurorPoolRepository, times(1)).save(any());
+        verify(jurorResponseCommonRepositoryMod, times(1))
+            .findByJurorNumber(jurorNumber);
+
+        final ArgumentCaptor<PaperResponse> PaperResponseCaptor = ArgumentCaptor.forClass(PaperResponse.class);
+
+        verify(jurorPaperResponseRepository, times(1))
+            .save(PaperResponseCaptor.capture());
+
+        PaperResponse paperResponse = PaperResponseCaptor.getValue();
+        assertThat(paperResponse.getJurorNumber()).isEqualTo(jurorNumber);
+        assertThat(paperResponse.getProcessingStatus()).isEqualTo(ProcessingStatus.CLOSED);
+        assertThat(paperResponse.getProcessingComplete()).isTrue();
+        assertThat(paperResponse.getCompletedAt()).isNotNull();
+
+        final ArgumentCaptor<Juror> jurorArgumentCaptor = ArgumentCaptor.forClass(Juror.class);
+
+        verify(jurorRepository, times(1))
+            .save(jurorArgumentCaptor.capture());
+
+        Juror juror = jurorArgumentCaptor.getValue();
+        assertThat(juror.getJurorNumber()).isEqualTo(jurorNumber);
+        assertThat(juror.getExcusalCode()).isEqualTo("D");
+        assertThat(juror.getExcusalDate()).isEqualTo(LocalDate.now(ZoneId.systemDefault()));
+        assertThat(juror.isResponded()).isTrue();
+
+        verify(contactCodeRepository, times(1)).findById(any());
+        verify(contactLogRepository, times(1)).saveAndFlush(any());
     }
 
+    @Test
+    void markJurorAsDeceasedDigitalResponseExists() {
+        String owner = "400";
+        String jurorNumber = "222222225";
 
-    //add positive tests for deceased excusal (with and without minimal paper response)
+        MarkAsDeceasedDto markAsDeceasedDto = new MarkAsDeceasedDto();
+
+        markAsDeceasedDto.setJurorNumber(jurorNumber);
+        markAsDeceasedDto.setDeceasedComment("Brother phoned in to say juror is deceased.");
+        markAsDeceasedDto.setPaperResponseExists(false); // this is the default value
+
+        JurorPool jurorPool = createValidJurorPool(jurorNumber, owner);
+        List<JurorPool> jurorPools = new ArrayList<>();
+        jurorPools.add(jurorPool);
+
+        doReturn(jurorPools).when(jurorPoolRepository)
+            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(jurorNumber, true);
+
+        ContactEnquiryType enquiryType = new ContactEnquiryType(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND),
+            "description");
+
+        Optional<ContactEnquiryType> enquiryTypeOpt = Optional.of(enquiryType);
+
+        doReturn(enquiryTypeOpt).when(contactEnquiryTypeRepository)
+            .findById(ContactEnquiryCode.valueOf(UNABLE_TO_ATTEND));
+
+        ContactCode contactCode = new ContactCode(
+            IContactCode.UNABLE_TO_ATTEND.getCode(),
+            IContactCode.UNABLE_TO_ATTEND.getDescription());
+        when(contactCodeRepository.findById(
+            IContactCode.UNABLE_TO_ATTEND.getCode())).thenReturn(Optional.of(contactCode));
+        
+        DigitalResponse jurorResponse = new DigitalResponse();
+        jurorResponse.setJurorNumber(jurorNumber);
+
+        doReturn(jurorResponse).when(jurorResponseCommonRepositoryMod)
+            .findByJurorNumber(jurorNumber);
+        doReturn(null).when(jurorDigitalResponseRepository)
+            .save(any(DigitalResponse.class));
+
+        deceasedResponseService.markAsDeceased(buildPayload(owner), markAsDeceasedDto);
+
+        verify(jurorPoolRepository, times(1))
+            .findByJurorJurorNumberAndIsActiveOrderByPoolReturnDateDesc(any(), anyBoolean());
+        verify(jurorPoolRepository, times(1)).save(any());
+        verify(jurorResponseCommonRepositoryMod, times(1))
+            .findByJurorNumber(jurorNumber);
+
+        verify(jurorDigitalResponseRepository, times(1))
+            .save(any(DigitalResponse.class));
+
+        final ArgumentCaptor<DigitalResponse> digitalResponseCaptor = ArgumentCaptor.forClass(DigitalResponse.class);
+
+        verify(jurorDigitalResponseRepository, times(1))
+            .save(digitalResponseCaptor.capture());
+
+        DigitalResponse digitalResponse = digitalResponseCaptor.getValue();
+        assertThat(digitalResponse.getJurorNumber()).isEqualTo(jurorNumber);
+        assertThat(digitalResponse.getProcessingStatus()).isEqualTo(ProcessingStatus.CLOSED);
+        assertThat(digitalResponse.getProcessingComplete()).isTrue();
+        assertThat(digitalResponse.getCompletedAt()).isNotNull();
+
+        final ArgumentCaptor<Juror> jurorArgumentCaptor = ArgumentCaptor.forClass(Juror.class);
+
+        verify(jurorRepository, times(1))
+            .save(jurorArgumentCaptor.capture());
+
+        Juror juror = jurorArgumentCaptor.getValue();
+        assertThat(juror.getJurorNumber()).isEqualTo(jurorNumber);
+        assertThat(juror.getExcusalCode()).isEqualTo("D");
+        assertThat(juror.getExcusalDate()).isEqualTo(LocalDate.now(ZoneId.systemDefault()));
+        assertThat(juror.isResponded()).isTrue();
+
+        verify(jurorPaperResponseRepository, times(0))
+            .findByJurorNumber(anyString());
+        verify(contactCodeRepository, times(1)).findById(any());
+        verify(contactLogRepository, times(1)).saveAndFlush(any());
+    }
 
     private BureauJwtPayload buildPayload(String owner) {
         return BureauJwtPayload.builder()
