415ad7822f64b23b73e38c7c9cb9e0cf72379f59
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2024-05-02 12:25:33 +0100

    Hotfix/jm 7150 (#293)
    * Update JurorRepositoryImpl.java
    
    * Update JurorRecordController_searchForJurorRecords.sql
    
    * hotfix/JM-7150

--- src/integration-test/resources/db/JurorRecordController_searchForJurorRecords.sql
+++ src/integration-test/resources/db/JurorRecordController_searchForJurorRecords.sql
@@ -1,9 +1,10 @@
 INSERT INTO juror_mod.pool
 (pool_no, "owner", return_date, total_no_required, no_requested, pool_type, loc_code, new_request)
 VALUES ('416220901', '416', '2022-12-03', 5, 5, 'CRO', '416', 'N'),
-('416220902', '400', '2022-12-03', 5, 5, 'CRO', '416', 'N'),
+('416220902', '416', '2022-12-03', 5, 5, 'CRO', '416', 'N'),
 ('415220901', '415', '2022-12-03', 5, 5, 'CRO', '415', 'N'),
-('767220901', '415', '2022-12-03', 5, 5, 'CRO', '767', 'N');
+('767220901', '415', '2022-12-03', 5, 5, 'CRO', '767', 'N'),
+('767220902', '400', '2022-12-03', 5, 5, 'CRO', '767', 'N');
 
 INSERT INTO juror_mod.juror (juror_number, title, last_name, first_name, dob, address_line_1, address_line_4, postcode, responded)
 VALUES ('641600091', 'MR','LNAMEFIVEFOURZERO','FNAMEFIVEFOURZERO', '1989-03-31', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true),
@@ -17,11 +18,12 @@ VALUES ('641600091', 'MR','LNAMEFIVEFOURZERO','FNAMEFIVEFOURZERO', '1989-03-31',
 ('641500097', 'MR','Lnamenineseven','Fnamenineseven', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true),
 ('641500098', 'MR','Lnamenineeight','Fnamenineeight', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true),
 ('641500099', 'MR','Lnameninenine','Fnameninenine', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true),
-('641500100', 'MR','Lnamenineten','Fnamenineten', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true);
+('641500100', 'MR','Lnamenineten','Fnamenineten', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true),
+('641500101', 'MR','Lnamenineten','Fnamenineten', '1988-01-01', '543 STREET NAME', 'ANYTOWN', 'CH1 2AN', true);
 
 INSERT INTO juror_mod.juror_pool (owner, juror_number, pool_number, is_active, status)
-VALUES ('400', '641600091', '416220901', false, 7),
-('400', '641600091', '416220902', true, 2),
+VALUES ('416', '641600091', '416220901', false, 7),
+('416', '641600091', '416220902', true, 2),
 ('416', '641600090', '416220901', true, 2),
 ('415', '641500091', '415220901', true, 2),
 ('415', '641500092', '415220901', true, 2),
@@ -32,4 +34,5 @@ VALUES ('400', '641600091', '416220901', false, 7),
 ('415', '641500097', '415220901', true, 2),
 ('415', '641500098', '415220901', true, 2),
 ('415', '641500099', '415220901', true, 2),
-('415', '641500100', '415220901', true, 2);
+('415', '641500100', '415220901', true, 2),
+('400', '641500101', '767220902', true, 2);
