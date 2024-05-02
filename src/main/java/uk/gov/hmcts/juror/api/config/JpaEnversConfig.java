bfabc865acfcdf63cf866eea137aa3506bee27b3
Craig Chapman <craig.chapman@cgi.com>
2024-05-02 08:42:59 +0100

    Move new config annotation
    - Move JPA Auditing annotation to a new JPA config class

---
+++ src/main/java/uk/gov/hmcts/juror/api/config/JpaEnversConfig.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.juror.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaEnversConfig {
+}
