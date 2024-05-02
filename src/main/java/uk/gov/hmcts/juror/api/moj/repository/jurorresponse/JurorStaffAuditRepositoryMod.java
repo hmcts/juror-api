a314efe4f11a427ce2da2343c9873aadc38469a6
Craig Chapman <craig.chapman@cgi.com>
2024-05-02 14:00:32 +0100

    Remove redundant bureau code
    - remove StaffJurorResponseAudit.java
    - remove StaffJurorResponseAuditKey.java
    - remove StaffJurorResponseAuditRepository.java

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/jurorresponse/JurorStaffAuditRepositoryMod.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/jurorresponse/JurorStaffAuditRepositoryMod.java
@@ -3,11 +3,11 @@ package uk.gov.hmcts.juror.api.moj.repository.jurorresponse;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.juror.api.bureau.domain.StaffJurorResponseAuditKey;
 import uk.gov.hmcts.juror.api.moj.domain.jurorresponse.StaffJurorResponseAuditMod;
+import uk.gov.hmcts.juror.api.moj.repository.staff.StaffJurorResponseAuditKeyMod;
 
 @Repository
 public interface JurorStaffAuditRepositoryMod extends CrudRepository<StaffJurorResponseAuditMod,
-    StaffJurorResponseAuditKey>,
+    StaffJurorResponseAuditKeyMod>,
     QuerydslPredicateExecutor<StaffJurorResponseAuditMod> {
 }
