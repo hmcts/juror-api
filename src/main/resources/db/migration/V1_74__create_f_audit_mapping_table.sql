b99b393f98f9acee72bf1617d69f92afb388e235
Craig Chapman <craig.chapman@cgi.com>
2024-05-13 16:36:20 +0100

    Update V1_74__create_f_audit_mapping_table.sql
    - update data types

--- src/main/resources/db/migration/V1_74__create_f_audit_mapping_table.sql
+++ src/main/resources/db/migration/V1_74__create_f_audit_mapping_table.sql
@@ -1,7 +1,7 @@
 create table juror_mod.f_audit_mapping (
 	loc_code varchar(3) not null,
-	heritage_f_audit varchar(9) not null,
-	f_audit varchar(9) null,
+	heritage_f_audit varchar(11) not null,
+	f_audit int(8) null,
 	constraint f_audit_mapping_pk primary key (loc_code, heritage_f_audit),
 	constraint f_audit_mapping_court_location_fk foreign key (loc_code) references juror_mod.court_location(loc_code)
 );
