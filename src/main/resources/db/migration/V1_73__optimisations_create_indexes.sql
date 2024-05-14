e5dab863adf0d784c912a23f73d43e40bcb50132
Phil Head <phil.head@cgi.com>
2024-05-14 20:06:31 +0100

    Update V1_73__optimisations_create_indexes.sql
    added juror pool index (juror number, owner)

--- src/main/resources/db/migration/V1_73__optimisations_create_indexes.sql
+++ src/main/resources/db/migration/V1_73__optimisations_create_indexes.sql
@@ -2,3 +2,4 @@
 DROP INDEX juror_response_staff_login_idx;
 CREATE INDEX juror_response_staff_login_idx ON juror_mod.juror_response (staff_login ASC);
 CREATE INDEX appearance_attendance_date_idx ON juror_mod.appearance USING btree (attendance_date, loc_code);
+CREATE INDEX juror_pool_juror_number_owner_idx on juror_mod.juror_pool (juror_number, owner);
