6bf8ce0e772f841ce23024009a9dd8a16d311c60
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-05-16 09:37:43 +0100

    update test data for unconfirmed attendance

--- src/integration-test/resources/db/mod/reports/UnconfirmedAttendanceReportITest_typical.sql
+++ src/integration-test/resources/db/mod/reports/UnconfirmedAttendanceReportITest_typical.sql
@@ -1,6 +1,7 @@
 INSERT INTO juror_mod.pool (owner, pool_no, return_date, total_no_required, no_requested, pool_type, loc_code,
                             new_request, attend_time)
-VALUES ('415', '415240101', current_date, 20, 20, 'CRO', '415', 'N', current_date);
+VALUES ('415', '415240101', current_date, 20, 20, 'CRO', '415', 'N', current_date),
+       ('415', '415240102', current_date, 20, 20, 'CRO', '415', 'N', current_date);
 
 
 INSERT INTO juror_mod.juror (juror_number, first_name, last_name, dob, address_line_1, postcode, responded)
@@ -13,7 +14,13 @@ VALUES ('041500001', 'CName1', 'CSurname1', '1980-01-01', 'addressLine1', 'CH1 2
        ('041500007', 'CName7', 'CSurname7', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
        ('041500008', 'CName8', 'CSurname8', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
        ('041500009', 'CName9', 'CSurname9', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
-       ('041500010', 'CName10', 'CSurname10', '1980-01-01', 'addressLine1', 'CH1 2AN', true);
+       ('041500010', 'CName10', 'CSurname10', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500011', 'CName11', 'CSurname11', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500012', 'CName12', 'CSurname12', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500013', 'CName13', 'CSurname13', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500014', 'CName14', 'CSurname14', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500015', 'CName15', 'CSurname15', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500016', 'CName16', 'CSurname16', '1980-01-01', 'addressLine1', 'CH1 2AN', true);
 
 
 INSERT INTO juror_mod.juror_pool (owner, juror_number, pool_number, status, is_active, next_date)
@@ -26,7 +33,13 @@ VALUES ('415', '041500001', '415240101', 2, true, current_date),
        ('415', '041500007', '415240101', 2, true, current_date),
        ('415', '041500008', '415240101', 2, true, current_date),
        ('415', '041500009', '415240101', 2, true, current_date),
-       ('415', '041500010', '415240101', 2, true, current_date);
+       ('415', '041500010', '415240101', 2, true, current_date),
+       ('415', '041500011', '415240102', 2, true, current_date),
+       ('415', '041500012', '415240102', 2, true, current_date),
+       ('415', '041500013', '415240102', 2, true, current_date),
+       ('415', '041500014', '415240102', 2, true, current_date),
+       ('415', '041500015', '415240102', 2, true, current_date),
+       ('415', '041500016', '415240102', 2, true, current_date);
 
 
 INSERT INTO juror_mod.judge (id, owner, code, description)
@@ -34,7 +47,8 @@ VALUES (1, '415', 'JUD1', 'The Judge');
 INSERT INTO juror_mod.courtroom (id, loc_code, room_number, description)
 VALUES (1, '415', '1', 'The Courtroom');
 INSERT INTO juror_mod.trial (trial_number, loc_code, trial_start_date, trial_type, description, courtroom, judge)
-VALUES ('111111', '415', current_date, 'CRI', 'The Trial', 1, 1);
+VALUES ('111111', '415', current_date, 'CRI', 'The Trial', 1, 1),
+       ('111112', '415', current_date, 'CRI', 'The Trial', 1, 1);
 
 
 INSERT INTO juror_mod.appearance (attendance_date, juror_number, pool_number, loc_code, time_in, time_out, trial_number,
@@ -45,6 +59,9 @@ VALUES (current_date - 1, '041500001', '415240101', '415', '09:00:00', '17:30:00
        (current_date - 1, '041500004', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date - 1, '041500005', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date - 1, '041500010', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
+       (current_date - 1, '041500011', '415240102', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
+       (current_date - 1, '041500012', '415240102', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
+       (current_date - 1, '041500013', '415240102', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date, '041500001', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date, '041500002', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date, '041500003', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
@@ -54,4 +71,8 @@ VALUES (current_date - 1, '041500001', '415240101', '415', '09:00:00', '17:30:00
        (current_date, '041500007', '415240101', '415', '09:00:00', NULL, '111111', 'CHECKED_IN'),
        (current_date, '041500008', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
        (current_date, '041500009', '415240101', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
-       (current_date, '041500010', '415240101', '415', NULL, NULL, NULL, NULL);
+       (current_date, '041500010', '415240101', '415', NULL, NULL, NULL, NULL),
+       (current_date, '041500011', '415240102', '415', '09:00:00', '17:30:00', '111111', 'CHECKED_OUT'),
+       (current_date, '041500012', '415240102', '415', NULL, NULL, NULL, NULL),
+       (current_date, '041500013', '415240102', '415', '09:00:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500014', '415240102', '415', '09:00:00', NULL, '111111', 'CHECKED_IN');
