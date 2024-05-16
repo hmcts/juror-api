d2d38f0a44489c7577266f099e722908173e538d
benedwards <ben.edwards@cgi.com>
2024-05-16 23:34:14 +0100

    Added support for UnpaidAttendanceReportDetailedReport

---
+++ src/main/java/uk/gov/hmcts/juror/api/moj/report/grouped/groupby/GroupByAppearanceTrialNumberOrPoolNumber.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.juror.api.moj.report.grouped.groupby;
+
+import lombok.EqualsAndHashCode;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.GroupByResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.GroupedTableData;
+import uk.gov.hmcts.juror.api.moj.report.DataType;
+import uk.gov.hmcts.juror.api.moj.report.IDataType;
+import uk.gov.hmcts.juror.api.moj.report.IReportGroupBy;
+import uk.gov.hmcts.juror.api.moj.report.ReportGroupBy;
+
+import java.util.Collection;
+import java.util.List;
+
+@EqualsAndHashCode
+@SuppressWarnings("PMD.LawOfDemeter")
+public class GroupByAppearanceTrialNumberOrPoolNumber implements IReportGroupBy {
+    @Override
+    public String getGroupFunction(GroupedTableData groupedTableData) {
+        Object trialNumber = groupedTableData.get(DataType.APPEARANCE_TRIAL_NUMBER.getId());
+        if (trialNumber != null) {
+            return "Trial " + trialNumber;
+        }
+        return "Pool " + groupedTableData.get(DataType.APPEARANCE_POOL_NUMBER.getId());
+    }
+
+    @Override
+    public IReportGroupBy getNested() {
+        return ReportGroupBy.builder()
+            .dataType(DataType.ATTENDANCE_DATE)
+            .removeGroupByFromResponse(true)
+            .build();
+    }
+
+    @Override
+    public GroupByResponse getGroupedByResponse() {
+        return GroupByResponse.builder()
+            .name("TRIAL_NUMBER_OR_POOL_NUMBER")
+            .build();
+    }
+
+    @Override
+    public Collection<? extends IDataType> getRequiredDataTypes() {
+        return List.of(DataType.APPEARANCE_TRIAL_NUMBER,
+            DataType.APPEARANCE_POOL_NUMBER,
+            DataType.ATTENDANCE_DATE);
+    }
+
+    @Override
+    public Collection<String> getKeysToRemove() {
+        return List.of(DataType.APPEARANCE_TRIAL_NUMBER.getId(), DataType.APPEARANCE_POOL_NUMBER.getId(),
+            DataType.ATTENDANCE_DATE.getId());
+    }
+}
