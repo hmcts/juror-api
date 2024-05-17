1810bbc77e139f439d3b22fb480a43706d7d2edc
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-05-17 23:10:13 +0100

    Fixed issue with migration script failing to migrate existing data

--- src/main/resources/db/migration/V1_78__added_loc_code_to_financial_audit_primary_key.sql
+++ src/main/resources/db/migration/V1_78__added_loc_code_to_financial_audit_primary_key.sql
@@ -1,28 +1,27 @@
 ALTER table juror_mod.financial_audit_details_appearances
-    add column loc_code varchar(3) NOT NULL default 'ERR',
-    add CONSTRAINT financial_audit_details_appearances_app_loc_code_fk
-        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code),
-    add CONSTRAINT financial_audit_details_appearances_id_loc_code_unique UNIQUE (financial_audit_id, loc_code, attendance_date, appearance_version)
+    add column loc_code varchar(3) NOT NULL default 'ERR'
 ;
 
 update juror_mod.financial_audit_details_appearances fada
 set loc_code = fad.loc_code
 from juror_mod.financial_audit_details fad
 where fad.id = fada.financial_audit_id
-  and fada.loc_code is null;
+  and fada.loc_code = 'ERR';
 
+ 
+ ALTER table juror_mod.financial_audit_details_appearances
+    add CONSTRAINT financial_audit_details_appearances_app_loc_code_fk
+        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code),
+    add CONSTRAINT financial_audit_details_appearances_id_loc_code_unique UNIQUE (financial_audit_id, loc_code, attendance_date, appearance_version)
+;
 
 ALTER table juror_mod.appearance
-    add column f_audit_loc_code varchar(3),
-    add CONSTRAINT appearance_f_audit_loc_code_fk
-        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code)
+    add column f_audit_loc_code varchar(3)
 ;
 
 
 ALTER table juror_mod.appearance_audit
-    add column f_audit_loc_code varchar(3),
-    add CONSTRAINT appearance_f_audit_loc_code_fk
-        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code)
+    add column f_audit_loc_code varchar(3)
 ;
 
 update juror_mod.appearance fada
@@ -30,3 +29,23 @@ set f_audit_loc_code = fad.loc_code
 from juror_mod.financial_audit_details fad
 where fad.id = fada.f_audit
   and fada.f_audit_loc_code is null and fada.f_audit is not null;
+  
+ 
+ 
+ update juror_mod.appearance_audit fada
+set f_audit_loc_code = fad.loc_code
+from juror_mod.financial_audit_details fad
+where fad.id = fada.f_audit
+  and fada.f_audit_loc_code is null and fada.f_audit is not null;
+ 
+
+ ALTER table juror_mod.appearance
+    add CONSTRAINT appearance_f_audit_loc_code_fk
+        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code)
+;
+
+
+ALTER table juror_mod.appearance_audit
+    add CONSTRAINT appearance_f_audit_loc_code_fk
+        FOREIGN KEY (loc_code) REFERENCES juror_mod.court_location (loc_code)
+;
