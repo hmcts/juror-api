9d744c8039ce0a618eb75c7308042a2fa861d9cd
akikrahman1 <akhlaqur.rahman1@hmcts.net>
2024-05-17 10:19:08 +0100

    Update V1_68__create_utilisation_report.sql

--- src/main/resources/db/migration/V1_68__create_utilisation_report.sql
+++ src/main/resources/db/migration/V1_68__create_utilisation_report.sql
@@ -176,6 +176,7 @@ return query select
     min(case when jp.is_active = true then p.return_date else to_date('01/12/2099', 'dd/mm/yyyy') end ) return_date,
     greatest(least(coalesce(min(a.attendance_date), min(case when jp.is_active = true then p.return_date else to_date('01/12/2099', 'dd/mm/yyyy') end )),
     		min(case when jp.is_active = true then p.return_date else to_date('01/12/2099','dd/mm/yyyy') end )), p_start_date) service_start,
+    		-- figure out if an appearance occurred before service start date and use that if applicable
     least(min(coalesce(case when jp.status = 10 then jp.transfer_date else date(j.completion_date) end, p_end_date)),p_end_date) service_end,
     min(status) min_status
     from juror_mod.juror_pool jp
@@ -186,7 +187,7 @@ return query select
     left join juror_mod.appearance a
     on jp.juror_number = a.juror_number
     where p.loc_code = a.loc_code
-    and (((jp.status in (2,3,4,10) or (jp.status = 7 and a.attendance_date is not null)) and jp.is_active ='Y')
+    and (((jp.status in (2,3,4,10) or (jp.status = 7 and a.attendance_date is not null)) and jp.is_active = true)
           or (jp.status = 8 and a.attendance_date is not null and j.completion_date is not null))
     and p.loc_code = p_loc_code
     and ((jp.status = 10 and jp.transfer_date >= p_start_date)
