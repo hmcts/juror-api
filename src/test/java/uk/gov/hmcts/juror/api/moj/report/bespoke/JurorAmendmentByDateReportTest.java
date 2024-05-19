d30788399d955081f902f36cf78ec400fa0d0e3e
benedwards <ben.edwards@cgi.com>
2024-05-19 21:25:55 +0100

    Added more unit tests

---
+++ src/test/java/uk/gov/hmcts/juror/api/moj/report/bespoke/JurorAmendmentByDateReportTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.juror.api.moj.report.bespoke;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.AbstractReportResponse;
+import uk.gov.hmcts.juror.api.moj.report.AbstractReportTestSupport;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JurorAmendmentByDateReportTest extends AbstractJurorAmendmentReportTestSupport<JurorAmendmentByDateReport> {
+
+
+    public JurorAmendmentByDateReportTest() {
+        super(JurorAmendmentByDateReport.RequestValidator.class);
+    }
+
+
+    @Override
+    protected StandardReportRequest getValidRequest() {
+        return StandardReportRequest.builder()
+            .reportType(JurorAmendmentByJurorReport.class.getName())
+            .fromDate(LocalDate.of(2024, 1, 1))
+            .toDate(LocalDate.of(2024, 1, 30))
+            .build();
+    }
+
+    @Override
+    public JurorAmendmentByDateReport createReport() {
+        return new JurorAmendmentByDateReport(jurorAuditService, jurorService, userService);
+    }
+
+    @Test
+    void positiveGetHeadings() {
+        StandardReportRequest request = getValidRequest();
+        JurorAmendmentByDateReport report = createReport();
+
+        Map<String, AbstractReportResponse.DataTypeValue> headings = report
+            .getHeadings(request);
+
+        validateReportCreated(headings);
+
+        assertThat(headings).hasSize(2);
+        assertThat(headings).containsExactlyInAnyOrderEntriesOf(
+            Map.of("date_from", AbstractReportResponse.DataTypeValue.builder()
+                .displayName("Date from")
+                    .value("2024-01-01")
+                    .dataType("LocalDate")
+                .build(),
+                "date_to", AbstractReportResponse.DataTypeValue.builder()
+                    .displayName("Date to")
+                    .value("2024-01-30")
+                    .dataType("LocalDate")
+                    .build()
+            )
+        );
+    }
+
+    @Test
+    void negativeRequestMissingFromDate() {
+        StandardReportRequest request = getValidRequest();
+        request.setFromDate(null);
+        assertValidationFails(request,
+            new AbstractReportTestSupport.ValidationFailure("fromDate", "must not be null"));
+    }
+
+
+    @Test
+    void negativeRequestMissingToDate() {
+        StandardReportRequest request = getValidRequest();
+        request.setToDate(null);
+        assertValidationFails(request,
+            new AbstractReportTestSupport.ValidationFailure("toDate", "must not be null"));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/juror/api/moj/report/bespoke/JurorAmendmentByJurorReportTest.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.juror.api.moj.report.bespoke;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.juror.api.TestConstants;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.AbstractReportResponse;
+import uk.gov.hmcts.juror.api.moj.report.AbstractReportTestSupport;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JurorAmendmentByJurorReportTest extends AbstractJurorAmendmentReportTestSupport<JurorAmendmentByJurorReport> {
+
+
+    public JurorAmendmentByJurorReportTest() {
+        super(JurorAmendmentByJurorReport.RequestValidator.class);
+    }
+
+
+    @Override
+    protected StandardReportRequest getValidRequest() {
+        return StandardReportRequest.builder()
+            .reportType(JurorAmendmentByJurorReport.class.getName())
+            .jurorNumber(TestConstants.VALID_JUROR_NUMBER)
+            .build();
+    }
+
+    @Override
+    public JurorAmendmentByJurorReport createReport() {
+        return new JurorAmendmentByJurorReport(jurorAuditService, jurorService, userService);
+    }
+
+    @Test
+    void positiveGetHeadings() {
+        StandardReportRequest request = getValidRequest();
+        String jurorName = "Test Name 123";
+        JurorAmendmentByJurorReport report = createReport();
+
+        Map<String, AbstractReportResponse.DataTypeValue> headings = report
+            .getHeadings(request, jurorName);
+
+       validateReportCreated(headings);
+
+        assertThat(headings).hasSize(2);
+        assertThat(headings).containsExactlyInAnyOrderEntriesOf(
+            Map.of("juror_number", AbstractReportResponse.DataTypeValue.builder()
+                    .displayName("Juror Number")
+                    .value(TestConstants.VALID_JUROR_NUMBER)
+                    .dataType("String")
+                    .build(),
+                "juror_name", AbstractReportResponse.DataTypeValue.builder()
+                    .displayName("Juror Name")
+                    .value(jurorName)
+                    .dataType("String")
+                    .build()
+            )
+        );
+    }
+
+    @Test
+    void negativeRequestMissingJurorNumber() {
+        StandardReportRequest request = getValidRequest();
+        request.setJurorNumber(null);
+        assertValidationFails(request,
+            new AbstractReportTestSupport.ValidationFailure("jurorNumber", "must not be null"));
+    }
+
+    @Test
+    void negativeRequestInvalidJurorNumber() {
+        StandardReportRequest request = getValidRequest();
+        request.setJurorNumber(TestConstants.INVALID_JUROR_NUMBER);
+        assertValidationFails(request,
+            new AbstractReportTestSupport.ValidationFailure("jurorNumber", "must match \"^\\d{9}$\""));
+    }
+}
