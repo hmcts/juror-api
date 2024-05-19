b91ff9e22a1f0edb2808c972541e546a9c9f44e4
benedwards <ben.edwards@cgi.com>
2024-05-19 21:50:40 +0100

    Added index

--- src/main/resources/db/migration/V1_86__juror_audit-update.sql
+++ src/main/resources/db/migration/V1_86__juror_audit-update.sql
@@ -11,3 +11,4 @@ CREATE INDEX juror_audit_last_update_idx ON juror_mod.juror_audit (last_update, 
 CREATE INDEX juror_trial_juror_number_idx ON juror_mod.juror_trial USING btree (juror_number);
 drop index juror_mod.juror_trial_loc_code_idx;
 CREATE INDEX juror_trial_loc_code_idx ON juror_mod.juror_trial USING btree (loc_code, juror_number);
+CREATE INDEX juror_pool_status_idx ON juror_mod.juror_pool (status,owner, is_active);
