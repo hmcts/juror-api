28832fb9df230e880d8398916e55d8f3dd6d449b
benedwards <ben.edwards@cgi.com>
2024-05-21 14:02:31 +0100

    Standardized date format

--- src/main/java/uk/gov/hmcts/juror/api/config/ApplicationBeans.java
+++ src/main/java/uk/gov/hmcts/juror/api/config/ApplicationBeans.java
@@ -1,6 +1,13 @@
 package uk.gov.hmcts.juror.api.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -8,6 +15,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.Executor;
 
@@ -41,5 +49,21 @@ public class ApplicationBeans {
     @Bean(name = "threadPoolTaskExecutor")
     public Executor threadPoolTaskExecutor() {
         return new ThreadPoolTaskExecutor();
+    }
+
+
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat("yyyy-MM-dd");
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ISO_DATE));
+            builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ISO_TIME));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+
+            //builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ISO_DATE));
+            //builder.deserializers(new LocalTimeDeserializer(DateTimeFormatter.ISO_TIME));
+            //builder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
+        };
     }
 }
