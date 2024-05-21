14b9e845aa1bebcd09534dcb7a954648393a80cb
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-05-21 16:15:57 +0100

    add integration tests

---
+++ src/integration-test/java/uk/gov/hmcts/juror/api/moj/report/standard/PanelMembersStatusReportITest.java
@@ -0,0 +1,224 @@
+package uk.gov.hmcts.juror.api.moj.report.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import uk.gov.hmcts.juror.api.TestConstants;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.AbstractReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardReportResponse;
+import uk.gov.hmcts.juror.api.moj.report.AbstractStandardReportControllerITest;
+import uk.gov.hmcts.juror.api.moj.report.ReportHashMap;
+import uk.gov.hmcts.juror.api.moj.report.ReportLinkedMap;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@Sql({
+    "/db/truncate.sql",
+    "/db/mod/truncate.sql",
+    "/db/administration/createUsers.sql",
+    "/db/mod/reports/PanelMembersStatusReportITest_Typical.sql"
+})
+public class PanelMembersStatusReportITest extends AbstractStandardReportControllerITest {
+
+    @Autowired
+    public PanelMembersStatusReportITest(TestRestTemplate template) {
+        super(template, PanelMembersStatusReport.class);
+    }
+
+    @Override
+    protected String getValidJwt() {
+        return getCourtJwt("415");
+    }
+
+    @Override
+    protected StandardReportRequest getValidPayload() {
+        return addReportType(StandardReportRequest.builder()
+            .trialNumber("111111")
+            .locCode(TestConstants.VALID_COURT_LOCATION)
+            .build());
+    }
+
+    @Test
+    void positiveTypicalCourt() {
+        testBuilder()
+            .triggerValid()
+            .responseConsumer(this::verifyAndRemoveReportCreated)
+            .assertEquals(getTypicalResponse());
+    }
+
+    @Test
+    void negativeUnauthorisedBureau() {
+        testBuilder()
+            .jwt(getBureauJwt())
+            .triggerInvalid()
+            .assertMojForbiddenResponse("User not allowed to access this report");
+    }
+
+    @Test
+    void negativeMissingTrialNumber() {
+        StandardReportRequest request = getValidPayload();
+        request.setTrialNumber(null);
+
+        testBuilder()
+            .payload(addReportType(request))
+            .jwt(getValidJwt())
+            .triggerInvalid()
+            .assertInvalidPathParam("trialNumber: must not be blank");
+    }
+
+    @Test
+    void positiveNoData() {
+        StandardReportRequest request = getValidPayload();
+        request.setTrialNumber("111112");
+
+        AbstractReportResponse.TableData<List<LinkedHashMap<String, Object>>> tableData = new StandardReportResponse
+                                                                                                  .TableData<>();
+        tableData.setHeadings(List.of(
+            StandardReportResponse.TableData.Heading.builder()
+                .id("juror_number")
+                .name("Juror Number")
+                .dataType("String")
+                .headings(null)
+                .build(),
+            StandardReportResponse.TableData.Heading.builder()
+                .id("panel_status")
+                .name("Panel Status")
+                .dataType("String")
+                .headings(null)
+                .build()
+        ));
+        tableData.setData(List.of());
+
+        testBuilder()
+            .payload(addReportType(request))
+            .jwt(getValidJwt())
+            .triggerValid()
+            .responseConsumer(this::verifyAndRemoveReportCreated)
+            .assertEquals(StandardReportResponse.builder()
+                .headings(getResponseHeadings("111112"))
+                .tableData(tableData)
+                .build());
+    }
+
+    private StandardReportResponse getTypicalResponse() {
+        return StandardReportResponse.builder()
+            .headings(getResponseHeadings("111111"))
+            .tableData(getResponseTableData())
+            .build();
+    }
+
+    private ReportHashMap<String, StandardReportResponse.DataTypeValue> getResponseHeadings(String trialNumber) {
+        ReportHashMap<String, StandardReportResponse.DataTypeValue> headingsMap = new ReportHashMap<>();
+
+        headingsMap.add("names", StandardReportResponse.DataTypeValue.builder()
+            .displayName("Names")
+            .dataType("String")
+            .value("CName1, CName2, CName3, et al")
+            .build());
+        headingsMap.add("trial_number", StandardReportResponse.DataTypeValue.builder()
+            .displayName("Trial Number")
+            .dataType("String")
+            .value(trialNumber)
+            .build());
+        headingsMap.add("court_room", StandardReportResponse.DataTypeValue.builder()
+            .displayName("Court Room")
+            .dataType("String")
+            .value("The Courtroom")
+            .build());
+        headingsMap.add("judge", StandardReportResponse.DataTypeValue.builder()
+            .displayName("Judge")
+            .dataType("String")
+            .value("The Judge")
+            .build());
+        headingsMap.add("court_name", StandardReportResponse.DataTypeValue.builder()
+            .displayName("Court Name")
+            .dataType("String")
+            .value("CHESTER (415)")
+            .build());
+
+        return headingsMap;
+    }
+
+    private StandardReportResponse.TableData<List<LinkedHashMap<String, Object>>> getResponseTableData() {
+        return StandardReportResponse.TableData.<List<LinkedHashMap<String, Object>>>builder()
+            .headings(List.of(
+                StandardReportResponse.TableData.Heading.builder()
+                    .id("juror_number")
+                    .name("Juror Number")
+                    .dataType("String")
+                    .headings(null)
+                    .build(),
+                StandardReportResponse.TableData.Heading.builder()
+                    .id("panel_status")
+                    .name("Panel Status")
+                    .dataType("String")
+                    .headings(null)
+                    .build()
+            ))
+            .data(List.of(
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500001")
+                    .add("panel_status", "Challenged"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500002")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500003")
+                    .add("panel_status", "Not Used"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500004")
+                    .add("panel_status", "Challenged"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500005")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500006")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500007")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500008")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500009")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500010")
+                    .add("panel_status", "Returned"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500011")
+                    .add("panel_status", "Returned"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500012")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500013")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500014")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500015")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500016")
+                    .add("panel_status", "Returned"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500017")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500018")
+                    .add("panel_status", "Juror"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500019")
+                    .add("panel_status", "Not Used"),
+                new ReportLinkedMap<String, Object>()
+                    .add("juror_number", "041500020")
+                    .add("panel_status", "Challenged")
+            )).build();
+    }
+}
---
+++ src/integration-test/resources/db/mod/reports/PanelMembersStatusReportITest_Typical.sql
@@ -0,0 +1,107 @@
+INSERT INTO juror_mod.pool (owner, pool_no, return_date, total_no_required, no_requested, pool_type, loc_code,
+                            new_request, attend_time)
+VALUES ('415', '415240101', current_date, 20, 20, 'CRO', '415', 'N', current_date);
+
+
+INSERT INTO juror_mod.juror (juror_number, first_name, last_name, dob, address_line_1, postcode, responded)
+VALUES ('041500001', 'CName1', 'CSurname1', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500002', 'CName2', 'CSurname2', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500003', 'CName3', 'CSurname3', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500004', 'CName4', 'CSurname4', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500005', 'CName5', 'CSurname5', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500006', 'CName6', 'CSurname6', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500007', 'CName7', 'CSurname7', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500008', 'CName8', 'CSurname8', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500009', 'CName9', 'CSurname9', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500010', 'CName10', 'CSurname10', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500011', 'CName11', 'CSurname11', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500012', 'CName12', 'CSurname12', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500013', 'CName13', 'CSurname13', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500014', 'CName14', 'CSurname14', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500015', 'CName15', 'CSurname15', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500016', 'CName16', 'CSurname16', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500017', 'CName17', 'CSurname17', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500018', 'CName18', 'CSurname18', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500019', 'CName19', 'CSurname19', '1980-01-01', 'addressLine1', 'CH1 2AN', true),
+       ('041500020', 'CName20', 'CSurname20', '1980-01-01', 'addressLine1', 'CH1 2AN', true);
+
+
+INSERT INTO juror_mod.juror_pool (owner, juror_number, pool_number, status, is_active, next_date)
+VALUES ('415', '041500001', '415240101', 2, true, current_date),
+       ('415', '041500002', '415240101', 2, true, current_date),
+       ('415', '041500003', '415240101', 2, true, current_date),
+       ('415', '041500004', '415240101', 2, true, current_date),
+       ('415', '041500005', '415240101', 2, true, current_date),
+       ('415', '041500006', '415240101', 2, true, current_date),
+       ('415', '041500007', '415240101', 2, true, current_date),
+       ('415', '041500008', '415240101', 2, true, current_date),
+       ('415', '041500009', '415240101', 2, true, current_date),
+       ('415', '041500010', '415240101', 2, true, current_date),
+       ('415', '041500011', '415240101', 2, true, current_date),
+       ('415', '041500012', '415240101', 2, true, current_date),
+       ('415', '041500013', '415240101', 2, true, current_date),
+       ('415', '041500014', '415240101', 2, true, current_date),
+       ('415', '041500015', '415240101', 2, true, current_date),
+       ('415', '041500016', '415240101', 2, true, current_date),
+       ('415', '041500017', '415240101', 2, true, current_date),
+       ('415', '041500018', '415240101', 2, true, current_date),
+       ('415', '041500019', '415240101', 2, true, current_date),
+       ('415', '041500020', '415240101', 2, true, current_date);
+
+
+INSERT INTO juror_mod.judge (id, owner, code, description)
+VALUES (1, '415', 'JUD1', 'The Judge');
+INSERT INTO juror_mod.courtroom (id, loc_code, room_number, description)
+VALUES (1, '415', '1', 'The Courtroom');
+INSERT INTO juror_mod.trial (trial_number, loc_code, trial_start_date, trial_type, description, courtroom, judge)
+VALUES ('111111', '415', current_date, 'CRI', 'CName1, CName2, CName3, et al', 1, 1);
+INSERT INTO juror_mod.trial (trial_number, loc_code, trial_start_date, trial_type, description, courtroom, judge)
+VALUES ('111112', '415', current_date, 'CRI', 'CName1, CName2, CName3, et al', 1, 1);
+
+
+INSERT INTO juror_mod.appearance (attendance_date, juror_number, pool_number, loc_code, time_in, time_out, trial_number,
+                                  appearance_stage)
+VALUES (current_date, '041500001', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500002', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500003', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500004', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500005', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500006', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500007', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500008', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500009', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500010', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500011', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500012', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500013', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500014', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500015', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500016', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500017', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500018', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500019', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN'),
+       (current_date, '041500020', '415240101', '415', '09:30:00', NULL, '111111', 'CHECKED_IN');
+
+
+INSERT INTO juror_mod.juror_trial (loc_code, juror_number, trial_number, date_selected, result, completed,
+                                   empanelled_date, return_date)
+VALUES ('415', '041500001', '111111', current_date, 'CD', true, current_date, NULL),
+       ('415', '041500002', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500003', '111111', current_date, 'NU', true, current_date, NULL),
+       ('415', '041500004', '111111', current_date, 'CD', true, current_date, NULL),
+       ('415', '041500005', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500006', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500007', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500008', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500009', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500010', '111111', current_date, 'R', true, current_date, current_date),
+       ('415', '041500011', '111111', current_date, 'R', true, current_date, current_date),
+       ('415', '041500012', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500013', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500014', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500015', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500016', '111111', current_date, 'R', true, current_date, current_date),
+       ('415', '041500017', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500018', '111111', current_date, 'J', true, current_date, NULL),
+       ('415', '041500019', '111111', current_date, 'NU', true, current_date, NULL),
+       ('415', '041500020', '111111', current_date, 'CD', true, current_date, NULL);
