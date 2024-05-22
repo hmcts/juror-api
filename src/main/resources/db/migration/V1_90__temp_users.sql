dae461ab7b4da6d58f4672ab31e54980ba9fdd6c
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-05-22 11:29:12 +0100

    update name

--- src/main/resources/db/migration/V1_90__temp_users.sql
+++ src/main/resources/db/migration/V1_90__temp_users.sql
@@ -1,7 +1,7 @@
 INSERT INTO juror_mod.users (user_type, username, email, name, active)
 VALUES
        ('ADMINISTRATOR', 'neil.perry', 'neil.perry@hmcts.net', 'Neil Perry', true),
-       ('ADMINISTRATOR', 'rjicardo.freitasrocha', 'ricardo.freitasrocha@hmcts.net', 'Ricardo Freitasrocha', true),
+       ('ADMINISTRATOR', 'ricardo.rocha', 'ricardo.freitasrocha@hmcts.net', 'Ricardo Rocha', true),
        ('ADMINISTRATOR', 'akhlaqur.rahman1', 'akhlaqur.rahman1@hmcts.net', 'Akhlaqur Rahman', true),
        ('ADMINISTRATOR', 'cade.faulkner', 'cade.faulkner@hmcts.net', 'Cade Faulkner', true);
 
@@ -9,6 +9,6 @@ VALUES
 INSERT INTO juror_mod.user_courts (username, loc_code)
 VALUES
        ('neil.perry', '400'),
-       ('ricardo.freitasrocha', '400'),
+       ('ricardo.rocha', '400'),
        ('akhlaqur.rahman1', '400'),
        ('cade.faulkner', '400');
