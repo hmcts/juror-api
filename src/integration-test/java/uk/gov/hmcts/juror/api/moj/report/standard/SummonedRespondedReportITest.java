63ed01fad5467a1da04b6b5a4d5b77a2d10dc8a9
gregs-cgi <144363053+gregs-cgi@users.noreply.github.com>
2024-05-24 16:47:26 +0100

    Feat (JM-7322) add Summoned and Responded Pool Members Report (#383)
    * Feat (JM-7322) add Summoned and Responded Pool Members Report
    
    * Test (JM-7322) update integration tests for new SQL

---
+++ src/integration-test/java/uk/gov/hmcts/juror/api/moj/report/standard/SummonedRespondedReportITest.java
@@ -0,0 +1,177 @@
+package uk.gov.hmcts.juror.api.moj.report.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardTableData;
+import uk.gov.hmcts.juror.api.moj.report.AbstractStandardReportControllerITest;
+import uk.gov.hmcts.juror.api.moj.report.ReportHashMap;
+import uk.gov.hmcts.juror.api.moj.report.ReportLinkedMap;
+
+import java.util.List;
+
+@Sql({
+    "/db/truncate.sql",
+    "/db/mod/truncate.sql",
+    "/db/administration/createUsers.sql",
+    "/db/mod/reports/CurrentPoolStatusReportControllerITest_typical.sql"
+})
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")//False positive
+class SummonedRespondedReportITest extends AbstractStandardReportControllerITest {
+
+    @Autowired
+    public SummonedRespondedReportITest(TestRestTemplate template) {
+        super(template, SummonedRespondedReport.class);
+    }
+
+    @Override
+    protected String getValidJwt() {
+        return getBureauJwt();
+    }
+
+    @Override
+    protected StandardReportRequest getValidPayload() {
+        return addReportType(StandardReportRequest.builder()
+            .poolNumber("415230101")
+            .build());
+    }
+
+
+    @Test
+    void negativeTypicalCourt() {
+        testBuilder()
+            .jwt(getCourtJwt("415"))
+            .triggerInvalid()
+            .assertMojForbiddenResponse("User not allowed to access this report");
+    }
+
+    @Test
+    void positiveTypicalBureau() {
+        testBuilder()
+            .jwt(getBureauJwt())
+            .triggerValid()
+            .responseConsumer(this::verifyAndRemoveReportCreated)
+            .assertEquals(getTypicalResponse());
+    }
+
+    @Test
+    void negativeInvalidPayload() {
+        StandardReportRequest request = getValidPayload();
+        request.setPoolNumber(null);
+        testBuilder()
+            .payload(addReportType(request))
+            .triggerInvalid()
+            .assertInvalidPathParam("poolNumber: must not be null");
+    }
+
+    private StandardReportResponse getTypicalResponse() {
+        return StandardReportResponse.builder()
+            .headings(new ReportHashMap<String, StandardReportResponse.DataTypeValue>()
+                .add("pool_number", StandardReportResponse.DataTypeValue.builder()
+                    .displayName("Pool number")
+                    .dataType("String")
+                    .value("415230101")
+                    .build()))
+            .tableData(
+                StandardReportResponse.TableData.<StandardTableData>builder()
+                    .headings(List.of(
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("juror_number")
+                            .name("Juror Number")
+                            .dataType("String")
+                            .headings(null)
+                            .build(),
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("first_name")
+                            .name("First Name")
+                            .dataType("String")
+                            .headings(null)
+                            .build(),
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("last_name")
+                            .name("Last Name")
+                            .dataType("String")
+                            .headings(null)
+                            .build(),
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("juror_postal_address")
+                            .name("Address")
+                            .dataType("List")
+                            .headings(List.of(
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_address_line_1")
+                                    .name("Address Line 1")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build(),
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_address_line_2")
+                                    .name("Address Line 2")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build(),
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_address_line_3")
+                                    .name("Address Line 3")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build(),
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_address_line_4")
+                                    .name("Address Line 4")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build(),
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_address_line_5")
+                                    .name("Address Line 5")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build(),
+                                StandardReportResponse.TableData.Heading.builder()
+                                    .id("juror_postcode")
+                                    .name("Postcode")
+                                    .dataType("String")
+                                    .headings(null)
+                                    .build()))
+                            .build(),
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("juror_postcode")
+                            .name("Postcode")
+                            .dataType("String")
+                            .headings(null)
+                            .build(),
+                        StandardReportResponse.TableData.Heading.builder()
+                            .id("summoned_responded")
+                            .name("Responded")
+                            .dataType("Boolean")
+                            .headings(null)
+                            .build()))
+                    .data(StandardTableData.of(
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "641500020")
+                            .add("first_name", "John0")
+                            .add("last_name", "Smith0")
+                            .add("juror_postal_address",
+                                 new ReportLinkedMap<String, Object>()
+                                     .add("juror_address_line_1", "addressLine1")
+                                     .add("juror_postcode", "A00 0AA"))
+                            .add("juror_postcode", "A00 0AA")
+                            .add("summoned_responded", false),
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "641500021")
+                            .add("first_name", "John1")
+                            .add("last_name", "Smith1")
+                            .add("juror_postal_address",
+                                 new ReportLinkedMap<String, Object>()
+                                     .add("juror_address_line_1", "addressLine1")
+                                     .add("juror_postcode", "A11 1AA"))
+                            .add("juror_postcode", "A11 1AA")
+                            .add("summoned_responded", true)))
+                    .build())
+            .build();
+    }
+}
