f656c3faea6152a72a36c3d169b62070da4b6895
benedwards <ben.edwards@cgi.com>
2024-05-25 23:06:09 +0100

    Added support for generating letters on the weekend

--- src/test/java/uk/gov/hmcts/juror/api/moj/xerox/DeferralLetterTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/xerox/DeferralLetterTest.java
@@ -4,16 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.juror.api.moj.domain.FormCode;
-import uk.gov.hmcts.juror.api.moj.exception.MojException;
 import uk.gov.hmcts.juror.api.moj.xerox.letters.DeferralLetter;
-import uk.gov.hmcts.juror.api.testsupport.DisableIfWeekend;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Calendar;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ExtendWith(SpringExtension.class)
 class DeferralLetterTest extends AbstractLetterTest {
@@ -85,23 +81,17 @@ class DeferralLetterTest extends AbstractLetterTest {
         final LocalDate date = LocalDate.of(2017, Month.FEBRUARY, 6);
         setupEnglishExpectedResult();
         DeferralLetter deferralLetter = new DeferralLetter(LetterTestUtils.testJurorPool(date),
-                                                        LetterTestUtils.testCourtLocation(),
-                                                        LetterTestUtils.testBureauLocation());
+            LetterTestUtils.testCourtLocation(),
+            LetterTestUtils.testBureauLocation());
 
-        int dayOfTheWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-        if (dayOfTheWeek == Calendar.SATURDAY || dayOfTheWeek == Calendar.SUNDAY) {
-            assertThatExceptionOfType(MojException.BusinessRuleViolation.class)
-                .isThrownBy(deferralLetter::getLetterString);
-        } else {
-            assertThat(deferralLetter.getLetterString()).isEqualTo(getExpectedEnglishResult());
-            assertThat(deferralLetter.getFormCode()).isEqualTo(FormCode.ENG_DEFERRAL.getCode());
-            assertThat(deferralLetter.getJurorNumber()).isEqualTo(LetterTestUtils.testJuror().getJurorNumber());
+        assertThat(deferralLetter.getLetterString()).isEqualTo(getExpectedEnglishResult());
+        assertThat(deferralLetter.getFormCode()).isEqualTo(FormCode.ENG_DEFERRAL.getCode());
+        assertThat(deferralLetter.getJurorNumber()).isEqualTo(LetterTestUtils.testJuror().getJurorNumber());
 
-            // Fax number is always empty
-            assertThat(deferralLetter.getData().get(11).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(12));
-            // Juror address 6 is always empty
-            assertThat(deferralLetter.getData().get(22).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(35));
-        }
+        // Fax number is always empty
+        assertThat(deferralLetter.getData().get(11).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(12));
+        // Juror address 6 is always empty
+        assertThat(deferralLetter.getData().get(22).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(35));
     }
 
     @Test
@@ -110,34 +100,27 @@ class DeferralLetterTest extends AbstractLetterTest {
         final LocalDate date = LocalDate.of(2017, Month.FEBRUARY, 6);
         setupWelshExpectedResult();
         DeferralLetter deferralLetter = new DeferralLetter(LetterTestUtils.testWelshJurorPool(date),
-                                                        LetterTestUtils.testCourtLocation(),
-                                                        LetterTestUtils.testBureauLocation(),
-                                                        LetterTestUtils.testWelshCourtLocation());
+            LetterTestUtils.testCourtLocation(),
+            LetterTestUtils.testBureauLocation(),
+            LetterTestUtils.testWelshCourtLocation());
 
-        int dayOfTheWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-        if (dayOfTheWeek == Calendar.SATURDAY || dayOfTheWeek == Calendar.SUNDAY) {
-            assertThatExceptionOfType(MojException.BusinessRuleViolation.class)
-                .isThrownBy(deferralLetter::getLetterString);
-        } else {
-            assertThat(deferralLetter.getLetterString()).isEqualTo(getExpectedWelshResult());
-            assertThat(deferralLetter.getFormCode()).isEqualTo(FormCode.BI_DEFERRAL.getCode());
-            assertThat(deferralLetter.getJurorNumber()).isEqualTo(LetterTestUtils.testWelshJuror().getJurorNumber());
+        assertThat(deferralLetter.getLetterString()).isEqualTo(getExpectedWelshResult());
+        assertThat(deferralLetter.getFormCode()).isEqualTo(FormCode.BI_DEFERRAL.getCode());
+        assertThat(deferralLetter.getJurorNumber()).isEqualTo(LetterTestUtils.testWelshJuror().getJurorNumber());
 
-            // Fax number is always empty
-            assertThat(deferralLetter.getData().get(11).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(12));
-            // Juror address 6 is always empty
-            assertThat(deferralLetter.getData().get(22).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(35));
-        }
+        // Fax number is always empty
+        assertThat(deferralLetter.getData().get(11).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(12));
+        // Juror address 6 is always empty
+        assertThat(deferralLetter.getData().get(22).getFormattedString()).isEqualTo(LetterTestUtils.emptyField(35));
     }
 
     @Test
-    @DisableIfWeekend
     void confirmWelshWithoutWelshCourtProducesEnglishOutput() {
         final LocalDate date = LocalDate.of(2017, Month.FEBRUARY, 6);
         setupEnglishExpectedResult();
         DeferralLetter deferralLetter = new DeferralLetter(LetterTestUtils.testWelshJurorPool(date),
-                                                        LetterTestUtils.testCourtLocation(),
-                                                        LetterTestUtils.testBureauLocation());
+            LetterTestUtils.testCourtLocation(),
+            LetterTestUtils.testBureauLocation());
         assertThat(deferralLetter.getLetterString()).isEqualTo(getExpectedEnglishResult());
     }
 }
