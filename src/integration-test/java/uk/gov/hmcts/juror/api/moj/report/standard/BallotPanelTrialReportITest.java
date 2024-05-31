c9eba867291f14feede555039f3040762a800b1e
Roonneeyy <cameron.rooney@HMCTS.net>
2024-05-31 09:02:32 +0100

    JM-6409-Trials (#384)
    * Added initial code copied from BallotPanelPoolReport
    
    * Updated to use QPanel
    
    * Anonymous conditional added
    
    * Added sql script with failing test
    
    * Added anonymous tests
    
    * Updated sql and integration tests
    
    * Formatting
    
    * Git comments resolved
    q
    
    * Git comments resolved
    
    * Formatting changes
    
    * Formatting changes

---
+++ src/integration-test/java/uk/gov/hmcts/juror/api/moj/report/standard/BallotPanelTrialReportITest.java
@@ -0,0 +1,155 @@
+package uk.gov.hmcts.juror.api.moj.report.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import uk.gov.hmcts.juror.api.TestConstants;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardTableData;
+import uk.gov.hmcts.juror.api.moj.report.AbstractStandardReportControllerITest;
+import uk.gov.hmcts.juror.api.moj.report.ReportHashMap;
+import uk.gov.hmcts.juror.api.moj.report.ReportLinkedMap;
+
+import java.util.List;
+
+@Sql({
+    "/db/mod/truncate.sql",
+    "/db/mod/reports/BallotPanelTrIalITest.sql"
+})
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")//False positive
+class BallotPanelTrialReportITest extends AbstractStandardReportControllerITest {
+    @Autowired
+    public BallotPanelTrialReportITest(TestRestTemplate template) {
+        super(template, BallotPanelTrialReport.class);
+    }
+
+    @Override
+    protected String getValidJwt() {
+        return getCourtJwt("415");
+    }
+
+    @Override
+    protected StandardReportRequest getValidPayload() {
+        return addReportType(StandardReportRequest.builder()
+            .trialNumber(TestConstants.VALID_TRIAL_NUMBER)
+            .build());
+    }
+
+    @Test
+    void positiveTypicalCourt() {
+        testBuilder()
+            .triggerValid()
+            .responseConsumer(this::verifyAndRemoveReportCreated)
+            .assertEquals(getTypicalResponse());
+    }
+
+    @Test
+    void positiveAnonymousCourt() {
+        StandardReportRequest request = getValidPayload();
+        request.setTrialNumber("T000000002");
+        testBuilder()
+            .payload(request)
+            .triggerValid()
+            .responseConsumer(this::verifyAndRemoveReportCreated)
+            .assertEquals(getAnonymousResponse());
+    }
+
+    @Test
+    void negativeInvalidPayload() {
+        StandardReportRequest request = getValidPayload();
+        request.setTrialNumber(null);
+        testBuilder()
+            .payload(request)
+            .triggerInvalid()
+            .assertInvalidPathParam("trialNumber: must not be blank");
+    }
+
+    @Test
+    void negativeUnauthorised() {
+        testBuilder()
+            .jwt(getBureauJwt())
+            .triggerInvalid()
+            .assertMojForbiddenResponse("User not allowed to access this report");
+    }
+
+    private StandardReportResponse getTypicalResponse() {
+        return StandardReportResponse.builder()
+            .headings(new ReportHashMap<>())
+            .tableData(
+                StandardReportResponse.TableData.<StandardTableData>builder()
+                    .headings(
+                        List.of(
+                            StandardReportResponse.TableData.Heading.builder()
+                                .id("juror_number")
+                                .name("Juror Number")
+                                .dataType("String")
+                                .headings(null)
+                                .build(),
+                            StandardReportResponse.TableData.Heading.builder()
+                                .id("first_name")
+                                .name("First Name")
+                                .dataType("String")
+                                .headings(null)
+                                .build(),
+                            StandardReportResponse.TableData.Heading.builder()
+                                .id("last_name")
+                                .name("Last Name")
+                                .dataType("String")
+                                .headings(null)
+                                .build(),
+                            StandardReportResponse.TableData.Heading.builder()
+                                .id("juror_postcode")
+                                .name("Postcode")
+                                .dataType("String")
+                                .headings(null)
+                                .build()
+                        ))
+                    .data(StandardTableData.of(
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000001")
+                            .add("first_name", "John1")
+                            .add("last_name", "Smith1")
+                            .add("juror_postcode", "AD1 2HP"),
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000002")
+                            .add("first_name", "John2")
+                            .add("last_name", "Smith2")
+                            .add("juror_postcode", "AD2 2HP"),
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000003")
+                            .add("first_name", "John3")
+                            .add("last_name", "Smith3")
+                            .add("juror_postcode", "AD3 2HP")
+                    ))
+                    .build())
+            .build();
+    }
+
+    private StandardReportResponse getAnonymousResponse() {
+        return StandardReportResponse.builder()
+            .headings(new ReportHashMap<>())
+            .tableData(
+                StandardReportResponse.TableData.<StandardTableData>builder()
+                    .headings(
+                        List.of(
+                            StandardReportResponse.TableData.Heading.builder()
+                                .id("juror_number")
+                                .name("Juror Number")
+                                .dataType("String")
+                                .headings(null)
+                                .build()
+                        ))
+                    .data(StandardTableData.of(
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000004"),
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000005"),
+                        new ReportLinkedMap<String, Object>()
+                            .add("juror_number", "200000006")
+                    ))
+                    .build())
+            .build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/juror/api/moj/report/standard/BallotPanelTrialReportTest.java
@@ -0,0 +1,103 @@
+package uk.gov.hmcts.juror.api.moj.report.standard;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import uk.gov.hmcts.juror.api.TestConstants;
+import uk.gov.hmcts.juror.api.TestUtils;
+import uk.gov.hmcts.juror.api.moj.controller.reports.request.StandardReportRequest;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.AbstractReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.StandardTableData;
+import uk.gov.hmcts.juror.api.moj.domain.trial.QPanel;
+import uk.gov.hmcts.juror.api.moj.report.AbstractStandardReportTestSupport;
+import uk.gov.hmcts.juror.api.moj.report.DataType;
+import uk.gov.hmcts.juror.api.moj.repository.PoolRequestRepository;
+import uk.gov.hmcts.juror.api.moj.repository.trial.TrialRepository;
+import uk.gov.hmcts.juror.api.moj.utils.SecurityUtil;
+
+import java.util.List;
+import java.util.Map;
+
+class BallotPanelTrialReportTest extends AbstractStandardReportTestSupport<BallotPanelTrialReport> {
+
+    private MockedStatic<SecurityUtil> securityUtilMockedStatic;
+
+    private TrialRepository trialRepository;
+
+    public BallotPanelTrialReportTest() {
+        super(
+            QPanel.panel,
+            BallotPanelTrialReport.RequestValidator.class,
+            DataType.JUROR_NUMBER,
+            DataType.FIRST_NAME,
+            DataType.LAST_NAME,
+            DataType.JUROR_POSTCODE
+        );
+        setHasPoolRepository(false);
+    }
+
+    @Override
+    public BallotPanelTrialReport createReport(PoolRequestRepository poolRequestRepository) {
+        return new BallotPanelTrialReport(trialRepository);
+    }
+
+    @BeforeEach
+    @Override
+    public void beforeEach() {
+        securityUtilMockedStatic = Mockito.mockStatic(SecurityUtil.class);
+        this.trialRepository = Mockito.mock(TrialRepository.class);
+        super.beforeEach();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        TestUtils.afterAll();
+        securityUtilMockedStatic.close();
+    }
+
+    @Override
+    protected StandardReportRequest getValidRequest() {
+        return StandardReportRequest.builder()
+            .reportType(report.getName())
+            .trialNumber(TestConstants.VALID_TRIAL_NUMBER)
+            .build();
+    }
+
+    @Override
+    public void positivePreProcessQueryTypical(JPAQuery<Tuple> query, StandardReportRequest request) {
+
+        request.setTrialNumber(TestConstants.VALID_TRIAL_NUMBER);
+        securityUtilMockedStatic.when(SecurityUtil::getCourts).thenReturn(List.of(TestConstants.VALID_COURT_LOCATION));
+        report.preProcessQuery(query, request);
+
+        Mockito.verify(query, Mockito.times(1))
+            .where(QPanel.panel.trial.trialNumber.eq(TestConstants.VALID_TRIAL_NUMBER));
+        Mockito.verify(query, Mockito.times(1))
+            .where(QPanel.panel.trial.courtLocation.locCode.in(List.of(TestConstants.VALID_COURT_LOCATION)));
+        Mockito.verify(query, Mockito.times(1))
+            .orderBy(QPanel.panel.juror.jurorNumber.asc());
+    }
+
+    @Override
+    public Map<String, AbstractReportResponse.DataTypeValue> positiveGetHeadingsTypical(
+        StandardReportRequest request,
+        AbstractReportResponse.TableData<StandardTableData> tableData,
+        StandardTableData data) {
+
+        Map<String, StandardReportResponse.DataTypeValue> map = report.getHeadings(request, tableData);
+        assertHeadingContains(map, request, false, Map.of());
+        return map;
+    }
+
+    @Test
+    void negativeMissingTrialNumber() {
+        StandardReportRequest request = getValidRequest();
+        request.setTrialNumber(null);
+        assertValidationFails(request, new ValidationFailure("trialNumber", "must not be blank"));
+    }
+}
