6a3d11a94d45ccc0b1acccafbb8e7543ab1bbeb4
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-06-07 13:17:03 +0100

    update trial start_date format on dto (#458)

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/trial/TrialSummaryDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/trial/TrialSummaryDto.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.juror.api.moj.controller.response.trial;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.juror.api.validation.ValidationConstants;
 
 import java.time.LocalDate;
 
@@ -32,6 +34,7 @@ public class TrialSummaryDto {
     private CourtroomsDto courtroomsDto;
 
     @JsonProperty("start_date")
+    @JsonFormat(pattern = ValidationConstants.DATE_FORMAT)
     private LocalDate trialStartDate;
 
     @JsonProperty("protected")
