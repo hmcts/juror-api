f772cb6bd8a13c6217d0f7b0c62481d7a4c8e16b
benedwards <ben.edwards@cgi.com>
2024-06-10 17:51:27 +0100

    Fixed styles

--- src/main/java/uk/gov/hmcts/juror/api/moj/domain/NotifyTemplateFieldMod.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/domain/NotifyTemplateFieldMod.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
