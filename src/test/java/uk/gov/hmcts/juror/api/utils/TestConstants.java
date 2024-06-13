71ac5feeadd93a34fc7f1c797b85f9ce0bed2a03
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-06-13 11:31:03 +0100

    Updated jwt expiry to be a long instead of a string (#490)
    * Updated jwt expiry to be a long instead of a string
    
    * Updated jwt expiry to be a long instead of a string
    
    * Bumping chart version/ fixing aliases
    
    * Updated expiry token
    
    * Fixed typo
    
    * Fixed test failure
    
    ---------
    
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/juror/api/utils/TestConstants.java
+++ src/test/java/uk/gov/hmcts/juror/api/utils/TestConstants.java
@@ -3,7 +3,8 @@ package uk.gov.hmcts.juror.api.utils;
 public class TestConstants {
     public static final String JWT = "GeneratedJwtFromSecret";
     public static final String JWT_SECRET = "dGhpcy1pcy1hLXRlc3Qta2V5LWZvci11cy13aGVuLWNyZWF0aW5nLXNlY3JldHM=";
-    public static final long JWT_EXPIRY = 36000;
+    public static final String JWT_EXPIRY_STR = "1m";
+    public static final long JWT_EXPIRY = 60000;
 
     protected TestConstants() {
 
