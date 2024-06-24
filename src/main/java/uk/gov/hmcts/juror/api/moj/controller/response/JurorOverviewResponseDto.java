e5893538c0952b19009fec20d75da9385191ef3d
benedwards <ben.edwards@cgi.com>
2024-06-24 12:35:53 +0100

    Fixed styles

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/JurorOverviewResponseDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/response/JurorOverviewResponseDto.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.juror.api.moj.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.OpenAPI31;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
