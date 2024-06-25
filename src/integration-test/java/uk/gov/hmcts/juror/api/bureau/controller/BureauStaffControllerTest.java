a297afd2d1da6a09f3d62ea0aff145f7b21640cf
benedwards <ben.edwards@cgi.com>
2024-06-25 15:35:26 +0100

    Fixed styles

--- src/integration-test/java/uk/gov/hmcts/juror/api/bureau/controller/BureauStaffControllerTest.java
+++ src/integration-test/java/uk/gov/hmcts/juror/api/bureau/controller/BureauStaffControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
