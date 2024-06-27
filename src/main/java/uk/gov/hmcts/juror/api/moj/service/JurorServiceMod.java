eca7d337e4e168f54352d7b1aab107d14165a183
benedwards <ben.edwards@cgi.com>
2024-06-27 11:40:34 +0100

    Fixed issue where pool transfers would break public authentication

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/JurorServiceMod.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/JurorServiceMod.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.juror.api.moj.service;
 
 import uk.gov.hmcts.juror.api.moj.domain.Juror;
 
+import java.util.Optional;
+
 public interface JurorServiceMod {
     Juror getJurorFromJurorNumber(String jurorNumber);
+
+    Optional<Juror> getJurorOptionalFromJurorNumber(String jurorNumber);
 }
