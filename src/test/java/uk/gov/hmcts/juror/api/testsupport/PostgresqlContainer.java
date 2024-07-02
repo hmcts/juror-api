232d9f06567778fb1e2696748dafc2d2f41c1b24
Ian Fisher - CGI <142427330+Ianrfisher@users.noreply.github.com>
2024-07-02 19:16:53 +0100

    change to ACR postgres image (#564)
    * change to ACR postgres image
    
    * make compatible with postgres image
    
    * change to alpine copy of image
    
    * remove compatibility work
    
    * switch to ACR cached image
    
    * set as final

--- src/test/java/uk/gov/hmcts/juror/api/testsupport/PostgresqlContainer.java
+++ src/test/java/uk/gov/hmcts/juror/api/testsupport/PostgresqlContainer.java
@@ -2,16 +2,19 @@ package uk.gov.hmcts.juror.api.testsupport;
 
 import org.flywaydb.core.Flyway;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 
 public class PostgresqlContainer extends PostgreSQLContainer<PostgresqlContainer> {
-
-    private static final String IMAGE_VERSION = "postgres:16-alpine";
+    
+    private static final DockerImageName dockerImageName = DockerImageName
+        .parse("hmctspublic.azurecr.io/imported/postgres:16-alpine")
+        .asCompatibleSubstituteFor("postgres");
     private static PostgresqlContainer container;
 
     private PostgresqlContainer() {
-        super(IMAGE_VERSION);
+        super(dockerImageName);
         setup();
     }
 
