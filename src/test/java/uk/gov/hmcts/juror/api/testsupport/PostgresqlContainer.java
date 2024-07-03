833b99326e803e92579cc5db56841dbac1147dd4
Ian Fisher - CGI <142427330+Ianrfisher@users.noreply.github.com>
2024-07-03 12:06:44 +0100

    try sds ACR (#567)

--- src/test/java/uk/gov/hmcts/juror/api/testsupport/PostgresqlContainer.java
+++ src/test/java/uk/gov/hmcts/juror/api/testsupport/PostgresqlContainer.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class PostgresqlContainer extends PostgreSQLContainer<PostgresqlContainer> {
     
     private static final DockerImageName dockerImageName = DockerImageName
-        .parse("hmctspublic.azurecr.io/imported/postgres:16-alpine")
+        .parse("sdshmctspublic.azurecr.io/imported/postgres:16-alpine")
         .asCompatibleSubstituteFor("postgres");
     private static PostgresqlContainer container;
 
