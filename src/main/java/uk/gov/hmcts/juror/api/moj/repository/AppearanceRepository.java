479bb18ff7666962f291ee1866176cf665f7222d
benedwards <ben.edwards@cgi.com>
2024-07-04 15:08:55 +0100

    Fixed issue where FTA check would count absents

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/AppearanceRepository.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/AppearanceRepository.java
@@ -75,7 +75,11 @@ public interface AppearanceRepository extends IAppearanceRepository, JpaReposito
     @Query(value = "select nextval('juror_mod.attendance_audit_seq')", nativeQuery = true)
     Long getNextAttendanceAuditNumber();
 
-    long countByJurorNumberAndAppearanceStageNotNull(String jurorNumber);
+    @Query(value = "select count(*) from Appearance a where a.jurorNumber= ?1 "
+        + "and a.appearanceStage is not null "
+        + "and( a.attendanceType is null or a.attendanceType not in (uk.gov.hmcts.juror.api.moj.enumeration.AttendanceType.ABSENT))")
+    long countNoneAbsentAttendances(String jurorNumber);
+
 
     Optional<Appearance> findByJurorNumberAndAttendanceDateAndAppearanceStage(String jurorNumber,
                                                                               LocalDate attendanceDate,
