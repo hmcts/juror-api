f389785a371f37dffed2a78a8191bd4735a1cfd6
benedwards <ben.edwards@cgi.com>
2024-07-23 23:02:10 +0100

    Fixed issue where, denying a deferral when a juror is in deferral maintenance would cause the juror to incorrectly be put back as responded

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/DeferralResponseServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/DeferralResponseServiceImpl.java
@@ -101,24 +101,27 @@ public class DeferralResponseServiceImpl implements DeferralResponseService {
     private void declineDeferralForJurorPool(BureauJwtPayload payload, DeferralRequestDto deferralRequestDto,
                                              JurorPool jurorPool) {
 
+        boolean isInDeferralMaintenance = jurorPool.getStatus().getStatus() == IJurorStatus.DEFERRED;
+
         String username = payload.getLogin();
+        //If juror is in deferral maintenance, we should not revert the existing (granted) deferral
+        if (!isInDeferralMaintenance) {
+            JurorStatus jurorStatus = new JurorStatus();
+            jurorStatus.setStatus(IJurorStatus.RESPONDED);
+            jurorPool.setStatus(jurorStatus);
+            jurorPool.setUserEdtq(username);
+            jurorPool.setDeferralCode(deferralRequestDto.getDeferralReason());
+            jurorPool.setDeferralDate(null);
+            jurorPool.setNextDate(jurorPool.getPool().getReturnDate());
+            jurorPoolRepository.save(jurorPool);
 
-        JurorStatus jurorStatus = new JurorStatus();
-        jurorStatus.setStatus(IJurorStatus.RESPONDED);
-        jurorPool.setStatus(jurorStatus);
-        jurorPool.setUserEdtq(username);
-        jurorPool.setDeferralCode(deferralRequestDto.getDeferralReason());
-        jurorPool.setDeferralDate(null);
-        jurorPool.setNextDate(jurorPool.getPool().getReturnDate());
-        jurorPoolRepository.save(jurorPool);
-
-        Juror juror = jurorPool.getJuror();
-        juror.setResponded(true);
-        juror.setUserEdtq(username);
-        juror.setExcusalRejected(DEFERRAL_REJECTED_CODE);
-        juror.setExcusalDate(null);
-        jurorRepository.save(juror);
-
+            Juror juror = jurorPool.getJuror();
+            juror.setResponded(true);
+            juror.setUserEdtq(username);
+            juror.setExcusalRejected(DEFERRAL_REJECTED_CODE);
+            juror.setExcusalDate(null);
+            jurorRepository.save(juror);
+        }
         // update Juror History - create deferral denied status event
         JurorHistory jurorHistory = JurorHistory.builder()
             .jurorNumber(jurorPool.getJurorNumber())
