34f58f7173de3d02d8971ae6d4b74337886c3096
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2024-08-07 15:43:16 +0100

    task/JM-8018 Show cause court letter (#687)
    * task/JM-8018 Show cause court letter
    
    * fixing integration tests
    
    * Update ShowCauseLetterListRepositoryImpl.java
    
    * updates following review

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/ShowCauseLetterListRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/ShowCauseLetterListRepositoryImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.juror.api.moj.repository.letter.court;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -7,11 +8,20 @@ import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.juror.api.moj.domain.IJurorStatus;
+import uk.gov.hmcts.juror.api.moj.domain.QAppearance;
+import uk.gov.hmcts.juror.api.moj.domain.QJuror;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorHistory;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorPool;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorStatus;
+import uk.gov.hmcts.juror.api.moj.domain.QPoolRequest;
 import uk.gov.hmcts.juror.api.moj.domain.letter.CourtLetterSearchCriteria;
-import uk.gov.hmcts.juror.api.moj.domain.letter.court.QShowCauseLetterList;
 import uk.gov.hmcts.juror.api.moj.domain.letter.court.ShowCauseLetterList;
+import uk.gov.hmcts.juror.api.moj.enumeration.AttendanceType;
+import uk.gov.hmcts.juror.api.moj.enumeration.HistoryCodeMod;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class ShowCauseLetterListRepositoryImpl implements ShowCauseLetterListRepository {
@@ -19,60 +29,101 @@ public class ShowCauseLetterListRepositoryImpl implements ShowCauseLetterListRep
     @PersistenceContext
     EntityManager entityManager;
 
-    private static final QShowCauseLetterList SHOW_CAUSE_LETTER_LIST = QShowCauseLetterList.showCauseLetterList;
-
     @Override
     public List<ShowCauseLetterList> findJurorsEligibleForShowCauseLetter(CourtLetterSearchCriteria searchCriteria,
                                                                           String owner) {
 
-        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
-        JPAQuery<ShowCauseLetterList> jpaQuery =
-            queryFactory.selectFrom(SHOW_CAUSE_LETTER_LIST).where(SHOW_CAUSE_LETTER_LIST.owner.eq(owner));
+        JPAQuery<Tuple> jpaQuery = buildBaseQuery(owner);
 
         filterEligibleLetterSearchCriteria(jpaQuery, searchCriteria);
 
         orderQueryResults(jpaQuery, searchCriteria.includePrinted());
 
-        return jpaQuery.fetch();
+        List<Tuple> results = jpaQuery.fetch();
+
+        return results.stream()
+            .map(tuple -> ShowCauseLetterList.builder()
+                .poolNumber(tuple.get(QPoolRequest.poolRequest.poolNumber))
+                .jurorNumber(tuple.get(QJuror.juror.jurorNumber))
+                .firstName(tuple.get(QJuror.juror.firstName))
+                .lastName(tuple.get(QJuror.juror.lastName))
+                .postcode(tuple.get(QJuror.juror.postcode))
+                .status(tuple.get(QJurorStatus.jurorStatus.statusDesc))
+                .absentDate(tuple.get(QAppearance.appearance.attendanceDate))
+                .datePrinted(tuple.get(QJurorHistory.jurorHistory.dateCreated))
+                .isActive(tuple.get(QJurorPool.jurorPool.isActive))
+                .build())
+            .collect(Collectors.toList());
     }
 
-    private void orderQueryResults(JPAQuery<ShowCauseLetterList> jpaQuery, boolean isIncludePrinted) {
+    private JPAQuery<Tuple> buildBaseQuery(String owner) {
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        return queryFactory.select(
+                QPoolRequest.poolRequest.poolNumber,
+                QJuror.juror.jurorNumber,
+                QJuror.juror.firstName,
+                QJuror.juror.lastName,
+                QJuror.juror.postcode,
+                QJurorStatus.jurorStatus.statusDesc,
+                QJurorHistory.jurorHistory.dateCreated,
+                QJurorPool.jurorPool.isActive,
+                QAppearance.appearance.attendanceDate)
+            .from(QJurorPool.jurorPool)
+            .join(QJuror.juror).on(QJuror.juror.eq(QJurorPool.jurorPool.juror))
+            .join(QPoolRequest.poolRequest).on(QPoolRequest.poolRequest.eq(QJurorPool.jurorPool.pool))
+            .join(QJurorStatus.jurorStatus).on(QJurorStatus.jurorStatus.eq(QJurorPool.jurorPool.status))
+            .join(QAppearance.appearance).on(QAppearance.appearance.jurorNumber.eq(QJuror.juror.jurorNumber))
+            .leftJoin(QJurorHistory.jurorHistory)
+            .on(QJurorHistory.jurorHistory.jurorNumber.eq(QJuror.juror.jurorNumber)
+                .and(QJurorHistory.jurorHistory.poolNumber.eq(QPoolRequest.poolRequest.poolNumber))
+                .and(QJurorHistory.jurorHistory.historyCode.eq(HistoryCodeMod.SHOW_CAUSE_LETTER)))
+            .where(QJurorPool.jurorPool.isActive.isTrue()
+                .and(QJurorPool.jurorPool.status.status.in(List.of(IJurorStatus.RESPONDED,
+                        IJurorStatus.FAILED_TO_ATTEND))
+                    .and(QAppearance.appearance.noShow.isTrue())
+                    .and(QAppearance.appearance.attendanceType.eq(AttendanceType.ABSENT))
+                    .and(QPoolRequest.poolRequest.owner.eq(owner))));
+    }
+
+    private void orderQueryResults(JPAQuery<Tuple> jpaQuery, boolean isIncludePrinted) {
 
         if (isIncludePrinted) {
             jpaQuery.orderBy(new CaseBuilder()
-                .when(SHOW_CAUSE_LETTER_LIST.datePrinted.isNull())
+                .when(QJurorHistory.jurorHistory.dateCreated.isNull())
                 .then(0)
                 .otherwise(1)
                 .asc());
         }
-
         jpaQuery
-            .orderBy(SHOW_CAUSE_LETTER_LIST.absentDate.desc())
-            .orderBy(SHOW_CAUSE_LETTER_LIST.jurorNumber.asc());
+            .orderBy(QAppearance.appearance.attendanceDate.desc())
+            .orderBy(QJuror.juror.jurorNumber.asc());
     }
 
-    private void filterEligibleLetterSearchCriteria(JPAQuery<ShowCauseLetterList> jpaQuery,
+    private void filterEligibleLetterSearchCriteria(JPAQuery<Tuple> jpaQuery,
                                                     CourtLetterSearchCriteria courtLetterSearchCriteria) {
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorNumber())) {
-            jpaQuery.where(SHOW_CAUSE_LETTER_LIST.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
+            jpaQuery.where(QJuror.juror.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorName())) {
-            jpaQuery.where(SHOW_CAUSE_LETTER_LIST.firstName.concat(" " + SHOW_CAUSE_LETTER_LIST.lastName)
+            jpaQuery.where(QJuror.juror.firstName.concat(" ").concat(QJurorPool.jurorPool.juror.lastName)
                 .containsIgnoreCase(courtLetterSearchCriteria.jurorName()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.postcode())) {
             jpaQuery.where(
-                SHOW_CAUSE_LETTER_LIST.postcode.trim().equalsIgnoreCase(courtLetterSearchCriteria.postcode().trim()));
+                QJuror.juror.postcode.trim()
+                    .eq(courtLetterSearchCriteria.postcode().trim().toUpperCase()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.poolNumber())) {
-            jpaQuery.where(SHOW_CAUSE_LETTER_LIST.poolNumber.startsWith(courtLetterSearchCriteria.poolNumber()));
+            jpaQuery.where(QPoolRequest.poolRequest.poolNumber.startsWith(courtLetterSearchCriteria.poolNumber()));
         }
 
         if (!courtLetterSearchCriteria.includePrinted()) {
-            jpaQuery.where(SHOW_CAUSE_LETTER_LIST.datePrinted.isNull());
+            jpaQuery.where(QJurorHistory.jurorHistory.dateCreated.isNull());
         }
     }
 }
