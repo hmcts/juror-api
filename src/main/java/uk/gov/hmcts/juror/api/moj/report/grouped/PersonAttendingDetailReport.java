68e5ef7299d52c698098976cd2d00ce93841b0ec
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-08-07 10:14:33 +0100

    Updated Person attending reports to only included summoned / responded jurors (Before it also included JUROR and PANEL) (#684)

--- src/main/java/uk/gov/hmcts/juror/api/moj/report/grouped/PersonAttendingDetailReport.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/report/grouped/PersonAttendingDetailReport.java
@@ -52,14 +52,10 @@ public class PersonAttendingDetailReport extends AbstractGroupedReport {
         query.where(QJurorPool.jurorPool.nextDate.eq(request.getDate()));
         query.where(QJurorPool.jurorPool.pool.courtLocation.locCode.eq(SecurityUtil.getLocCode()));
         if (request.getIncludeSummoned()) {
-            query.where(QJurorPool.jurorPool.status.status.in(IJurorStatus.SUMMONED,
-                                                              IJurorStatus.RESPONDED,
-                                                              IJurorStatus.PANEL,
-                                                              IJurorStatus.JUROR));
+            query.where(QJurorPool.jurorPool.status.status
+                .in(IJurorStatus.SUMMONED, IJurorStatus.RESPONDED));
         } else {
-            query.where(QJurorPool.jurorPool.status.status.in(IJurorStatus.RESPONDED,
-                                                              IJurorStatus.PANEL,
-                                                              IJurorStatus.JUROR));
+            query.where(QJurorPool.jurorPool.status.status.in(IJurorStatus.RESPONDED));
         }
         query.orderBy(QJurorPool.jurorPool.juror.lastName.asc());
     }
