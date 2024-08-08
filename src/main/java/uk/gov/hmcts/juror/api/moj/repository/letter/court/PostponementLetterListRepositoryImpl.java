f8556dfed5f0a2ceaef5b26016bc6647812f08a2
akikrahman1 <akhlaqur.rahman1@hmcts.net>
2024-08-08 11:44:10 +0100

    updates for styles and PMD

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/PostponementLetterListRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/PostponementLetterListRepositoryImpl.java
@@ -9,7 +9,13 @@ import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.juror.api.moj.domain.*;
+import uk.gov.hmcts.juror.api.moj.domain.IJurorStatus;
+import uk.gov.hmcts.juror.api.moj.domain.QExcusalCode;
+import uk.gov.hmcts.juror.api.moj.domain.QJuror;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorHistory;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorPool;
+import uk.gov.hmcts.juror.api.moj.domain.QJurorStatus;
+import uk.gov.hmcts.juror.api.moj.domain.QPoolRequest;
 import uk.gov.hmcts.juror.api.moj.domain.letter.CourtLetterSearchCriteria;
 import uk.gov.hmcts.juror.api.moj.domain.letter.court.PostponedLetterList;
 import uk.gov.hmcts.juror.api.moj.enumeration.ExcusalCodeEnum;
@@ -26,14 +32,12 @@ public class PostponementLetterListRepositoryImpl implements PostponementLetterL
     @PersistenceContext
     EntityManager entityManager;
 
-
-
     private static final QExcusalCode EXCUSAL_CODE = QExcusalCode.excusalCode;
+
     @Override
     public List<PostponedLetterList> findJurorsEligibleForPostponementLetter(CourtLetterSearchCriteria searchCriteria) {
 
         JPAQuery<Tuple> jpaQuery = buildBaseQuery();
-
 
         filterEligibleLetterSearchCriteria(jpaQuery, searchCriteria);
 
@@ -56,24 +60,22 @@ public class PostponementLetterListRepositoryImpl implements PostponementLetterL
                 .build())
             .collect(Collectors.toList());
 
-
         ConcurrentHashMap<String, PostponedLetterList> postponedLetterListMap = new ConcurrentHashMap<>();
         postponedLetterLists.forEach(postponedLetterList -> {
-        String jurorNumber = postponedLetterList.getJurorNumber();
-        if (postponedLetterListMap.containsKey(jurorNumber)) {
-            PostponedLetterList existingPostponedLetterList =
-                postponedLetterListMap.get(jurorNumber);
-            if (postponedLetterList.getDeferralDate()
-                .isAfter(existingPostponedLetterList.getDeferralDate())) {
+            String jurorNumber = postponedLetterList.getJurorNumber();
+            if (postponedLetterListMap.containsKey(jurorNumber)) {
+                PostponedLetterList existingPostponedLetterList =
+                    postponedLetterListMap.get(jurorNumber);
+                if (postponedLetterList.getDeferralDate()
+                    .isAfter(existingPostponedLetterList.getDeferralDate())) {
+                    postponedLetterListMap.put(jurorNumber, postponedLetterList);
+                }
+            } else {
                 postponedLetterListMap.put(jurorNumber, postponedLetterList);
             }
-        } else {
-            postponedLetterListMap.put(jurorNumber, postponedLetterList);
-        }
-    });
-
+        });
         return postponedLetterListMap.values().stream().toList();
-}
+    }
 
     private JPAQuery<Tuple> buildBaseQuery() {
 
@@ -98,30 +100,30 @@ public class PostponementLetterListRepositoryImpl implements PostponementLetterL
             .join(EXCUSAL_CODE.excusalCode).on(EXCUSAL_CODE.excusalCode.code.eq(QJurorPool.jurorPool.deferralCode))
             .leftJoin(QJurorHistory.jurorHistory)
             .on((QJurorHistory.jurorHistory.jurorNumber.eq(QJuror.juror.jurorNumber))
-                    .and(QJurorHistory.jurorHistory.poolNumber.eq(QPoolRequest.poolRequest.poolNumber))
-                    .and(QJurorHistory.jurorHistory.historyCode.eq(HistoryCodeMod.POSTPONED_LETTER))
-                    .and(QJurorHistory.jurorHistory.otherInformationDate.eq(QJurorPool.jurorPool.deferralDate))
-                    .and(QJurorHistory.jurorHistory.dateCreatedDateOnly
-                             .after(QJuror.juror.bureauTransferDate)))
+                .and(QJurorHistory.jurorHistory.poolNumber.eq(QPoolRequest.poolRequest.poolNumber))
+                .and(QJurorHistory.jurorHistory.historyCode.eq(HistoryCodeMod.POSTPONED_LETTER))
+                .and(QJurorHistory.jurorHistory.otherInformationDate.eq(QJurorPool.jurorPool.deferralDate))
+                .and(QJurorHistory.jurorHistory.dateCreatedDateOnly
+                    .after(QJuror.juror.bureauTransferDate)))
             .where(QJurorPool.jurorPool.status.status.eq(IJurorStatus.DEFERRED)
-                       .and(EXCUSAL_CODE.excusalCode.code.eq(ExcusalCodeEnum.P.getCode()))
-                       .and(QJurorPool.jurorPool.owner.eq(SecurityUtil.getActiveOwner())));
+                .and(EXCUSAL_CODE.excusalCode.code.eq(ExcusalCodeEnum.P.getCode()))
+                .and(QJurorPool.jurorPool.owner.eq(SecurityUtil.getActiveOwner())));
     }
 
     private void filterEligibleLetterSearchCriteria(JPAQuery<Tuple> jpaQuery,
                                                     CourtLetterSearchCriteria courtLetterSearchCriteria) {
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorNumber())) {
-            jpaQuery.where(QJurorPool.jurorPool.juror.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
+            jpaQuery.where(QJuror.juror.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorName())) {
-            jpaQuery.where(QJurorPool.jurorPool.juror.firstName.concat(" ").concat(QJurorPool.jurorPool.juror.lastName)
-                               .containsIgnoreCase(courtLetterSearchCriteria.jurorName()));
+            jpaQuery.where(QJuror.juror.firstName.concat(" ").concat(QJuror.juror.lastName)
+                .containsIgnoreCase(courtLetterSearchCriteria.jurorName()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.postcode())) {
             jpaQuery.where(
-                QJurorPool.jurorPool.juror.postcode.trim()
+                QJuror.juror.postcode.trim()
                     .eq(courtLetterSearchCriteria.postcode().trim().toUpperCase()));
         }
 
@@ -136,21 +138,21 @@ public class PostponementLetterListRepositoryImpl implements PostponementLetterL
         }
     }
 
-
     private void orderQueryResults(JPAQuery<Tuple> jpaQuery,
-                                                  boolean isIncludePrinted) {
+                                   boolean isIncludePrinted) {
 
         if (isIncludePrinted) {
             jpaQuery.orderBy(new CaseBuilder()
-                                 .when(QJurorHistory.jurorHistory.dateCreated.isNull())
-                                 .then(0)
-                                 .otherwise(1)
-                                 .asc());
+                .when(QJurorHistory.jurorHistory.dateCreated.isNull())
+                .then(0)
+                .otherwise(1)
+                .asc());
         }
 
-        jpaQuery.orderBy(QJurorPool.jurorPool.juror.jurorNumber.asc())
-            .orderBy(QJurorPool.jurorPool.deferralDate.desc());
+        jpaQuery.orderBy(QJurorPool.jurorPool.deferralDate.desc())
+            .orderBy(QJuror.juror.jurorNumber.asc());
     }
+
     private BooleanExpression currentlyDeferred() {
         return QJurorPool.jurorPool.deferralDate.isNotNull().and(QJurorPool.jurorPool.isActive.isTrue());
     }
--- src/test/java/uk/gov/hmcts/juror/api/moj/service/letter/CourtPostponementLetterServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/service/letter/CourtPostponementLetterServiceImplTest.java
@@ -26,7 +26,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
