39077663f17462612fad9ced8fb098fbb3f9c623
akikrahman1 <akhlaqur.rahman1@hmcts.net>
2024-08-08 11:21:15 +0100

    update following review

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/ExcusalRefusalLetterListRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/letter/court/ExcusalRefusalLetterListRepositoryImpl.java
@@ -24,11 +24,22 @@ import java.util.stream.Collectors;
 
 public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLetterListRepository {
 
+    public static final Comparator<ExcusalRefusedLetterList> DATE_PRINT_COMPARATOR = (o1, o2) -> {
+        if (o1.getDatePrinted() == null && o2.getDatePrinted() == null) {
+            return 0;
+        } else if (o1.getDatePrinted() == null) {
+            return -1;
+        } else if (o2.getDatePrinted() == null) {
+            return 1;
+        } else {
+            return o2.getDatePrinted().compareTo(o1.getDatePrinted());
+        }
+    };
+
     @PersistenceContext
     EntityManager entityManager;
 
     @Override
-    @SuppressWarnings("PMD.CognitiveComplexity")
     public List<ExcusalRefusedLetterList> findJurorsEligibleForExcusalRefusalLetter(
         CourtLetterSearchCriteria searchCriteria, String owner) {
 
@@ -54,6 +65,23 @@ public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLe
                 .build())
             .collect(Collectors.toList());
 
+        ConcurrentHashMap<String, ExcusalRefusedLetterList>
+            excusalRefusedLetterListMap = removeDuplicateJurorRecords(excusalRefusedLetterLists);
+
+        List<String> poolNumbers =
+            excusalRefusedLetterLists.stream().map(ExcusalRefusedLetterList::getPoolNumber).distinct().toList();
+
+        // run query to get the most recent excusal refused date for each juror
+        List<Tuple> printedDates = getDatePrinted(owner, poolNumbers);
+
+        updateDatePrinted(searchCriteria, excusalRefusedLetterListMap, printedDates);
+
+        // sort by date printed (need unprinted letters to be at the top)
+        return excusalRefusedLetterListMap.values().stream().sorted(DATE_PRINT_COMPARATOR).toList();
+    }
+
+    private static ConcurrentHashMap<String, ExcusalRefusedLetterList> removeDuplicateJurorRecords(
+        List<ExcusalRefusedLetterList> excusalRefusedLetterLists) {
         // for each juror select the most recent excusal refused date and remove the other entries
         ConcurrentHashMap<String, ExcusalRefusedLetterList> excusalRefusedLetterListMap = new ConcurrentHashMap<>();
         excusalRefusedLetterLists.forEach(excusalRefusedLetterList -> {
@@ -69,13 +97,12 @@ public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLe
                 excusalRefusedLetterListMap.put(jurorNumber, excusalRefusedLetterList);
             }
         });
+        return excusalRefusedLetterListMap;
+    }
 
-        List<String> poolNumbers =
-            excusalRefusedLetterLists.stream().map(ExcusalRefusedLetterList::getPoolNumber).distinct().toList();
-
-        // run query to get the most recent excusal refused date for each juror
-        List<Tuple> printedDates = getDatePrinted(owner, poolNumbers);
-
+    private static void updateDatePrinted(CourtLetterSearchCriteria searchCriteria,
+                                  ConcurrentHashMap<String, ExcusalRefusedLetterList> excusalRefusedLetterListMap,
+                                  List<Tuple> printedDates) {
         // update the excusal refused letter printed date for each juror
         printedDates.forEach(tuple -> {
             String jurorNumber = tuple.get(QJuror.juror.jurorNumber);
@@ -93,21 +120,6 @@ public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLe
                 }
             }
         });
-
-        Comparator<ExcusalRefusedLetterList> datePrintComparator = (o1, o2) -> {
-            if (o1.getDatePrinted() == null && o2.getDatePrinted() == null) {
-                return 0;
-            } else if (o1.getDatePrinted() == null) {
-                return -1;
-            } else if (o2.getDatePrinted() == null) {
-                return 1;
-            } else {
-                return o2.getDatePrinted().compareTo(o1.getDatePrinted());
-            }
-        };
-
-        // sort by date printed (need unprinted letters to be at the top)
-        return excusalRefusedLetterListMap.values().stream().sorted(datePrintComparator).toList();
     }
 
     private JPAQuery<Tuple> buildBaseQuery(String owner) {
@@ -166,17 +178,17 @@ public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLe
     private void filterEligibleLetterSearchCriteria(JPAQuery<Tuple> jpaQuery,
                                                     CourtLetterSearchCriteria courtLetterSearchCriteria) {
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorNumber())) {
-            jpaQuery.where(QJurorPool.jurorPool.juror.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
+            jpaQuery.where(QJuror.juror.jurorNumber.startsWith(courtLetterSearchCriteria.jurorNumber()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.jurorName())) {
-            jpaQuery.where(QJurorPool.jurorPool.juror.firstName.concat(" ").concat(QJurorPool.jurorPool.juror.lastName)
+            jpaQuery.where(QJuror.juror.firstName.concat(" ").concat(QJuror.juror.lastName)
                 .containsIgnoreCase(courtLetterSearchCriteria.jurorName()));
         }
 
         if (!StringUtils.isEmpty(courtLetterSearchCriteria.postcode())) {
             jpaQuery.where(
-                QJurorPool.jurorPool.juror.postcode.trim()
+                QJuror.juror.postcode.trim()
                     .eq(courtLetterSearchCriteria.postcode().trim().toUpperCase()));
         }
 
@@ -188,6 +200,6 @@ public class ExcusalRefusalLetterListRepositoryImpl implements IExcusalRefusalLe
     private void orderExcusalRefusedQueryResults(JPAQuery<Tuple> jpaQuery) {
 
         jpaQuery.orderBy(QJurorHistory.jurorHistory.dateCreatedDateOnly.desc(),
-            QJurorPool.jurorPool.juror.jurorNumber.asc());
+            QJuror.juror.jurorNumber.asc());
     }
 }
