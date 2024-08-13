a8809bb214c69a0000437954553ea4890d5bbde8
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-08-13 09:20:17 +0100

    Updated Bureau response processing to exclude court owned jurors (#700)

--- src/main/java/uk/gov/hmcts/juror/api/bureau/service/BureauOfficerAllocatedRepliesServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/bureau/service/BureauOfficerAllocatedRepliesServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.juror.api.bureau.controller.response.BureauBacklogCountData;
 import uk.gov.hmcts.juror.api.bureau.controller.response.BureauOfficerAllocatedData;
 import uk.gov.hmcts.juror.api.bureau.controller.response.BureauOfficerAllocatedResponses;
@@ -21,6 +22,7 @@ public class BureauOfficerAllocatedRepliesServiceImpl implements BureauOfficerAl
     private final JurorDigitalResponseRepositoryMod jurorResponseRepository;
 
     @Override
+    @Transactional
     public BureauOfficerAllocatedResponses getBackLogData() {
         Tuple backlogStats = jurorResponseRepository.getAssignRepliesStatistics();
 
--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/jurorresponse/JurorDigitalResponseRepositoryModImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/jurorresponse/JurorDigitalResponseRepositoryModImpl.java
@@ -6,7 +6,9 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.juror.api.juror.domain.ProcessingStatus;
+import uk.gov.hmcts.juror.api.moj.domain.QJuror;
 import uk.gov.hmcts.juror.api.moj.domain.QUser;
 import uk.gov.hmcts.juror.api.moj.domain.UserType;
 import uk.gov.hmcts.juror.api.moj.domain.jurorresponse.QDigitalResponse;
@@ -31,27 +33,32 @@ public class JurorDigitalResponseRepositoryModImpl implements IJurorDigitalRespo
                 digitalResponse.count().as("allReplies"))
             .from(digitalResponse)
             .where(digitalResponse.processingStatus.eq(ProcessingStatus.TODO))
+            .where(digitalResponse.juror.bureauTransferDate.isNull())
             .where(digitalResponse.staff.isNull());
         return query.fetchOne();
     }
 
     @Override
+    @Transactional
     public List<Tuple> getAssignRepliesStatisticForUsers() {
         JPAQuery<Tuple> query = getJpaQueryFactory().select(
                 user.username.as("login"),
                 user.name.as("name"),
                 new CaseBuilder()
-                    .when(digitalResponse.urgent.isFalse())
+                    .when(digitalResponse.urgent.isFalse().and(QJuror.juror.isNotNull()))
                     .then(1L).otherwise(0L).sum().as("nonUrgent"),
                 new CaseBuilder()
-                    .when(digitalResponse.urgent.isTrue())
+                    .when(digitalResponse.urgent.isTrue().and(QJuror.juror.isNotNull()))
                     .then(1L).otherwise(0L).sum().as("urgent"),
                 digitalResponse.count().as("allReplies")
             ).from(user)
             .where(user.userType.eq(UserType.BUREAU))
             .where(user.active.isTrue())
             .leftJoin(digitalResponse)
-            .on(user.eq(digitalResponse.staff).and(digitalResponse.processingStatus.eq(ProcessingStatus.TODO)))
+            .on(user.eq(digitalResponse.staff)
+                .and(digitalResponse.processingStatus.eq(ProcessingStatus.TODO)))
+            .leftJoin(QJuror.juror).on(QJuror.juror.jurorNumber.eq(digitalResponse.jurorNumber)
+                .and(QJuror.juror.bureauTransferDate.isNull()))
             .groupBy(user.username, user.name);
         return query.fetch();
     }
