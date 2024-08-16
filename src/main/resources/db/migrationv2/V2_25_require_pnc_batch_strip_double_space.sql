85f31306620181e6a4742797ff6c2443df955151
benedwards <ben.edwards@cgi.com>
2024-08-16 16:10:15 +0100

    Added regex check to post code

--- src/main/resources/db/migrationv2/V2_25_require_pnc_batch_strip_double_space.sql
+++ src/main/resources/db/migrationv2/V2_25_require_pnc_batch_strip_double_space.sql
@@ -8,4 +8,6 @@ AS SELECT j.police_check,
     upper(regexp_replace(j.postcode::text, '\s+'::text, ''::text)) AS post_code
    FROM juror_mod.juror j
      JOIN juror_mod.juror_pool jp ON jp.juror_number::text = j.juror_number::text
-  WHERE jp.status = 2 AND (j.police_check IS NULL OR (j.police_check::text <> ALL (ARRAY['UNCHECKED_MAX_RETRIES_EXCEEDED'::character varying::text, 'ELIGIBLE'::character varying::text, 'INELIGIBLE'::character varying::text]))) AND jp.owner::text = '400'::text AND jp.is_active = true;
+  WHERE jp.status = 2 AND (j.police_check IS NULL OR (j.police_check::text <> ALL (ARRAY['UNCHECKED_MAX_RETRIES_EXCEEDED'::character varying::text, 'ELIGIBLE'::character varying::text, 'INELIGIBLE'::character varying::text]))) AND jp.owner::text = '400'::text AND jp.is_active = true
+  and upper(regexp_replace(j.postcode::text, '\s+'::text, ''::text)) ~ '^[A-Z0-9]{5,8}$'
+;
