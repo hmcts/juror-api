95788e30f9babc2678e1ebf5323482946ad0f148
benedwards <ben.edwards@cgi.com>
2024-08-17 22:38:07 +0100

    Updated migrations

--- src/main/resources/db/migrationv2/V2_25__require_pnc_batch_strip_double_space.sql
+++ src/main/resources/db/migrationv2/V2_25__require_pnc_batch_strip_double_space.sql
@@ -11,5 +11,5 @@ AS SELECT j.police_check,
   WHERE jp.status = 2 AND (j.police_check IS NULL OR (j.police_check::text <> ALL (ARRAY['UNCHECKED_MAX_RETRIES_EXCEEDED'::character varying::text, 'ELIGIBLE'::character varying::text, 'INELIGIBLE'::character varying::text]))) AND jp.owner::text = '400'::text AND jp.is_active = true
   and upper(regexp_replace(j.postcode::text, '\s+'::text, ''::text)) ~ '^[A-Z0-9]{5,8}$'
 ;
-create index juror_fname_idx on juror_mod.juror lower(first_name);
-create index juror_lname_idx on juror_mod.juror lower(last_name);
+create index juror_fname_idx on juror_mod.juror (lower(first_name));
+create index juror_lname_idx on juror_mod.juror (lower(last_name));
