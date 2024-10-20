1067d911cd8b2da557225d100007bda15a2b91a4
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2024-10-20 20:29:59 +0100

    task JM-7747a switch from loc name to loc court name

---
+++ src/main/resources/db/migrationv2/V2_38__update_court_location_loc_court_name.sql
@@ -0,0 +1,37 @@
+-- Update the court location names to match the new names
+
+
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT PORTSMOUTH'	WHERE	loc_code = '447';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT  NORWICH'	WHERE	loc_code = '443';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'ROYAL COURTS OF JUSTICE'	WHERE	loc_code = '626';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'NEWPORT(ISLE OF WIGHT)'	WHERE	loc_code = '478';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT SNARESBROOK'	WHERE	loc_code = '453';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT KINGSTON-U-HULL'	WHERE	loc_code = '403';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT HUNTINGDON'	WHERE	loc_code = '796';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT WOLVERHAMPTON'	WHERE	loc_code = '421';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT HOVE'	WHERE	loc_code = '799';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT BOLTON'	WHERE	loc_code = '470';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT BOURNEMOUTH'	WHERE	loc_code = '406';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT PRESTON'	WHERE	loc_code = '448';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT WINCHESTER'	WHERE	loc_code = '465';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'LEWES SITTING AT CHICHESTER'	WHERE	loc_code = '416';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT INNER LONDON CROWN'	WHERE	loc_code = '440';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'MANCHESTER, MINSHULL STREET'	WHERE	loc_code = '436';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT HAVERFORDWEST'	WHERE	loc_code = '761';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT SALISBURY'	WHERE	loc_code =	'480';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'WARWICKSHIRE JUSTICE CENTRE'	WHERE	loc_code = '000';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT CIRENCESTER'	WHERE	loc_code = '415';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT BURY ST EDMUNDS'	WHERE	loc_code = '754';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT CARMARTHEN'	WHERE	loc_code = '756';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT REDDITCH COUNTY COURT'	WHERE	loc_code = '797';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT MIDDLESEX GUILDHALL'	WHERE	loc_code = '464';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'WARWICK SITTING AT LEAMINGTON SPA'	WHERE	loc_code = '463';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT BARROW-IN-FURNESS'	WHERE	loc_code = '751';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'JURY CENTRAL SUMMONING BUREAU'	WHERE	loc_code = '400';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT KIDDERMINSTER'	WHERE	loc_code = '798';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'BIRMINGHAM CIVIL JUSTICE CENTRE'	WHERE	loc_code = '127';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'CHELMSFORD CROWN COURT'	WHERE	loc_code = '414';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT NEWCASTLE UPON TYNE'	WHERE	loc_code = '439';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT MAIDSTONE'	WHERE	loc_code = '434';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CROWN COURT AT NEWPORT S.WALES'	WHERE	loc_code = '441';
+UPDATE 	juror_mod.court_location 	SET loc_court_name = 	'THE CENTRAL CRIMINAL CROWN COURT'	WHERE	loc_code = '413'
