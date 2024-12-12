70d5cf18611c7e1a07a14ca8efd7fab776224637
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2024-12-12 10:55:17 +0000

    Task/JS-150 As a jury officer I want to pay jurors the higher rate after 2 weeks of service (#828)
    * task/JS-150 Pay jurors the higher rate after 2 weeks of service
    
    * updated logic and tests
    
    accounting for edge cases
    
    * addressing PMD issues

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/jurormanagement/JurorAppearanceServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/jurormanagement/JurorAppearanceServiceImpl.java
@@ -758,21 +758,6 @@ public class JurorAppearanceServiceImpl implements JurorAppearanceService {
 
     }
 
-    private static UpdateAttendanceDto.CommonData validateDeleteRequest(UpdateAttendanceDto request) {
-        final UpdateAttendanceDto.CommonData commonData = request.getCommonData();
-
-        if (request.getJuror().size() > 1 ^ commonData.getSingleJuror().equals(Boolean.FALSE)) {
-            throw new MojException.BadRequest("Cannot delete multiple juror attendance records",
-                null);
-        }
-
-        if (!commonData.getStatus().equals(UpdateAttendanceStatus.DELETE)) {
-            throw new MojException.BadRequest("Cannot delete attendance records for status "
-                + commonData.getStatus(), null);
-        }
-        return commonData;
-    }
-
     private List<JurorsToDismissResponseDto.JurorsToDismissData> buildJurorsToDismissResponse(
         List<Tuple> jurorsToDismissTuples) {
         List<JurorsToDismissResponseDto.JurorsToDismissData> jurorsToDismissResponseData = new ArrayList<>();
@@ -1451,7 +1436,20 @@ public class JurorAppearanceServiceImpl implements JurorAppearanceService {
         final int dateToCheckWeek = dateToCheck.get(WeekFields.ISO.weekOfWeekBasedYear());
 
         // account for the first appearance week being in the last 2 weeks of the year
-        if (firstAppearanceWeek == 52 && dateToCheckWeek >= 2 || firstAppearanceWeek == 51 && dateToCheckWeek >= 1) {
+        if (firstAppearanceWeek == 52 && dateToCheckWeek >= 2
+            || firstAppearanceWeek <= 51 && firstAppearanceWeek > dateToCheckWeek) {
+            return true;
+        }
+
+        // account for 53 week years
+        if (firstAppearanceWeek == 53 && dateToCheckWeek >= 3) {
+            return true;
+        }
+
+        // scenario where the first appearance is in the first week of the year
+        // and the date to check is in the first week of the year but month is different
+        if (firstAppearanceWeek == 1 && dateToCheckWeek == 1
+            && firstAppearanceDate.getMonth().getValue() == 1 && dateToCheck.getMonth().getValue() == 12) {
             return true;
         }
 
