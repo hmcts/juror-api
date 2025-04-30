a82a461f71e7b07caca618775ae52c041fd2c24d
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2025-04-30 17:10:54 +0100

    JS-299d fix template mapping

---
+++ src/main/resources/db/migrationv2/V2_70__update_notify_template_mapping.sql
@@ -0,0 +1,7 @@
+update juror_mod.notify_template_mapping
+set template_name = '1ST_TEMP_COMMS_ENG'
+where template_id = 'b17e55bb-d170-49c1-a22b-cd21c55d8039';
+
+update juror_mod.notify_template_mapping
+set template_name = 'SENT_TO_COURT_TEMP_EMAIL_ENG'
+where template_id = 'b6915247-ff69-4740-a4b7-22505be25ef4';
