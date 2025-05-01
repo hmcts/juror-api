aebee5a0af6c0be86f818716b05ef8e7b3f97ea0
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2025-05-01 15:03:51 +0100

    JS-210 removed undelivered set to closed

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/UndeliverableResponseServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/UndeliverableResponseServiceImpl.java
@@ -27,57 +27,32 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class UndeliverableResponseServiceImpl implements UndeliverableResponseService {
-    private final JurorHistoryService jurorHistoryService;
-    private final JurorPoolService jurorPoolService;
+  private final JurorHistoryService jurorHistoryService;
+  private final JurorPoolService jurorPoolService;
 
-    private final JurorPaperResponseRepositoryMod jurorPaperResponseRepository;
-    private final JurorDigitalResponseRepositoryMod jurorDigitalResponseRepository;
+  @Override
+  @Transactional
+  public void markAsUndeliverable(List<String> jurorNumbers) {
 
-    private final JurorResponseCommonRepositoryMod jurorResponseCommonRepositoryMod;
+    final String username = SecurityUtil.getUsername();
+    final String owner = SecurityUtil.getActiveOwner();
 
-    private final JurorResponseAuditRepositoryMod jurorResponseAuditRepository;
+    for (String jurorNumber : jurorNumbers) {
+      log.debug(
+          "Begin processing mark as undeliverable for juror {} by user {}", jurorNumber, username);
 
-    @Override
-    @Transactional
-    public void markAsUndeliverable(List<String> jurorNumbers) {
+      JurorPool jurorPool = jurorPoolService.getJurorPoolFromUser(jurorNumber);
+      JurorPoolUtils.checkOwnershipForCurrentUser(jurorPool, owner);
 
-        final String username = SecurityUtil.getUsername();
-        final String owner = SecurityUtil.getActiveOwner();
+      JurorStatus jurorStatus = new JurorStatus();
+      jurorStatus.setStatus(IJurorStatus.UNDELIVERABLE);
 
-        for (String jurorNumber : jurorNumbers) {
-            log.debug("Begin processing mark as undeliverable for juror {} by user {}", jurorNumber, username);
+      jurorPool.setStatus(jurorStatus);
+      jurorPool.setUserEdtq(username);
+      jurorPool.setNextDate(null);
+      jurorHistoryService.createUndeliveredSummonsHistory(jurorPool);
+      jurorPoolService.save(jurorPool);
 
-            JurorPool jurorPool = jurorPoolService.getJurorPoolFromUser(jurorNumber);
-            JurorPoolUtils.checkOwnershipForCurrentUser(jurorPool, owner);
-
-            JurorStatus jurorStatus = new JurorStatus();
-            jurorStatus.setStatus(IJurorStatus.UNDELIVERABLE);
-
-            jurorPool.setStatus(jurorStatus);
-            jurorPool.setUserEdtq(username);
-            jurorPool.setNextDate(null);
-            jurorHistoryService.createUndeliveredSummonsHistory(jurorPool);
-            jurorPoolService.save(jurorPool);
-
-            // Update any response record to closed to prevent further processing
-            AbstractJurorResponse jurorResponse = jurorResponseCommonRepositoryMod.findByJurorNumber(jurorNumber);
-
-            if (jurorResponse != null) {
-
-                setResponseToClosed(jurorResponse);
-
-                if (jurorResponse.getReplyType().getType().equals(ReplyMethod.DIGITAL.getDescription())) {
-                    jurorDigitalResponseRepository.save((DigitalResponse) jurorResponse);
-                } else {
-                    jurorPaperResponseRepository.save((PaperResponse) jurorResponse);
-                }
-            }
-        }
     }
-
-    private void setResponseToClosed(AbstractJurorResponse jurorResponse) {
-        jurorResponse.setProcessingStatus(jurorResponseAuditRepository, ProcessingStatus.CLOSED);
-        jurorResponse.setProcessingComplete(true);
-        jurorResponse.setCompletedAt(LocalDateTime.now());
-    }
+  }
 }
