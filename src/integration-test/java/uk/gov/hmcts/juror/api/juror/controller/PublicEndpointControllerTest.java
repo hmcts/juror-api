f1cd5ea5e4e7134660ab08afe6c0f2d59ee6c735
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2025-09-12 10:35:20 +0100

    Task/JS-448 SpringBoot update (#976)
    * JS-448 SB update and dependencies
    
    * wip 448
    
    * JS-448 defensive coding remoteconfig
    
    * JS-448 update envers 3.5.2
    
    * JS-448 remove comments
    
    * JS-448 fix integration/units tests checkstyle
    
    * task/JS-4 Address issue with PathVariables not validating
    
    * updating tests
    
    * Update build.gradle
    
    * task/JS-474 Update the Bi Summons letter to have English date of letter (#973)
    
    * Task/JS-297  (#972)
    
    * task/JS-297 juror address on pending letters to be updated
    
    * Update JurorRecordServiceImpl.java
    
    * Added integration test
    
    * updates for checkStyles
    
    * Update JurorRecordServiceImpl.java
    
    * addressing PMD issues
    
    * Allow Bureau users to see court owned jurors on pool overview (#977)
    
    * JS-340 update payment to clob sp (#978)
    
    * Task/js 340 (#979)
    
    * JS-340 update payment to clob sp
    
    * JS-340 update flyway script
    
    * task/JS-532 Adding jurors on call to Court dashboard (#980)
    
    * task/JS-532 Adding jurors on call to Court dashboard
    
    * update for field name
    
    * JS-227 - Add Nil Pool search option (#981)
    
    * JS-227 - Add Nil Pool search option
    
    * JS-227 - Update Nil Pool search option to boolean type
    
    * Task/JS-297 (#982)
    
    * task/JS-297 juror address on pending letters to be updated
    
    * Update JurorRecordServiceImpl.java
    
    * Added integration test
    
    * updates for checkStyles
    
    * Update JurorRecordServiceImpl.java
    
    * update to address issue with extracted letters
    
    * JS-448 update BureauJwtPayload
    
    * JS-448 fix checkstyle
    
    ---------
    
    Co-authored-by: akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
    Co-authored-by: Callum Young <148960132+callum-r-young@users.noreply.github.com>
    Co-authored-by: Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>

--- src/integration-test/java/uk/gov/hmcts/juror/api/juror/controller/PublicEndpointControllerTest.java
+++ src/integration-test/java/uk/gov/hmcts/juror/api/juror/controller/PublicEndpointControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.juror.api.AbstractIntegrationTest;
@@ -43,7 +43,7 @@ import static org.mockito.Mockito.verify;
 @SuppressWarnings("PMD.ExcessiveImports")
 public class PublicEndpointControllerTest extends AbstractIntegrationTest {
 
-    @MockBean
+    @MockitoBean
     private JurorPersistenceService mockJurorPersistenceService;
 
     @Autowired
