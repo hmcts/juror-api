156175e44d045f981579fb8090a82713935a154e
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2025-09-26 13:57:30 +0100

    Task/js 548 (#997)
    * JS-548 change the sort order for the pools
    
    * JS-548 change sort order

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/ActivePoolsRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/ActivePoolsRepositoryImpl.java
@@ -140,28 +140,30 @@ public class ActivePoolsRepositoryImpl implements IActivePoolsRepository {
         // return PaginationUtil.toPaginatedList(
         PaginatedList<PoolRequestActiveDataDto> allResults =
             PaginationUtil.toPaginatedList(
-                query,
-                filterQuery,
-                PoolRequestedFilterQuery.SortField.POOL_NUMBER,
-                SortMethod.ASC,
+
+            query,
+            filterQuery,
+            PoolRequestedFilterQuery.SortField.POOL_NUMBER,
+            SortMethod.ASC,
                 data -> {
                     PoolRequest poolRequest = Objects.requireNonNull(data.get(POOL_REQUEST));
                     return PoolRequestActiveDataDto.builder()
-                      .poolNumber(poolRequest.getPoolNumber())
-                      .requestedFromBureau(poolRequest.getNumberRequested())
-                      .confirmedFromBureau(data.get(CONFIRMED_FROM_BUREAU))
-                      .courtName(poolRequest.getCourtLocation().getName())
-                      .poolType(poolRequest.getPoolType().getDescription())
-                      .attendanceDate(poolRequest.getReturnDate())
-                      .required(poolRequest.getTotalNoRequired())
-                      .build();
-                });
+                  .poolNumber(poolRequest.getPoolNumber())
+                  .requestedFromBureau(poolRequest.getNumberRequested())
+                  .confirmedFromBureau(data.get(CONFIRMED_FROM_BUREAU))
+                  .courtName(poolRequest.getCourtLocation().getName())
+                  .poolType(poolRequest.getPoolType().getDescription())
+                  .attendanceDate(poolRequest.getReturnDate())
+                  .required(poolRequest.getTotalNoRequired())
+                  .build();
+            });
+
         List<PoolRequestActiveDataDto> filtered = allResults.getData().stream()
             .filter(dto -> dto.getRequired() > 0)
             .filter(dto -> dto.getAttendanceDate() != null
                 &&
                 dto.getAttendanceDate().isBefore(LocalDate.now().plusDays(35)))
-            .sorted(Comparator.comparing(PoolRequestActiveDataDto::getRequired).reversed())
+            .sorted(Comparator.comparing(PoolRequestActiveDataDto::getAttendanceDate))
             .toList();
         return new PaginatedList<>(
             allResults.getCurrentPage(),
