dd8eec232c57bd5dab28bb7bed0e7be82e3b67cc
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2025-10-20 16:06:14 +0100

    Task/JS-533 Reassigned juror's showing in the pool count (#1019)
    * task/JS-533 Reassigned juror's showing in the pool count
    
    * Update ActivePoolsRepositoryImpl.java

--- src/main/java/uk/gov/hmcts/juror/api/moj/repository/ActivePoolsRepositoryImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/repository/ActivePoolsRepositoryImpl.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.juror.api.moj.repository;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -172,16 +173,22 @@ public class ActivePoolsRepositoryImpl implements IActivePoolsRepository {
         );
     }
 
-
-
-
     private PaginatedList<PoolRequestActiveDataDto> getActiveCourtTabRequests(ActivePoolFilterQuery filterQuery) {
         JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        // Sum up active jurors only
+        NumberExpression<Integer> activeJurorCountExpr =
+            new CaseBuilder()
+                .when(JUROR_POOL.isActive.isTrue())
+                .then(1)
+                .otherwise(0)
+                .sum()
+                .as("activeJurorCount");
 
         JPAQuery<Tuple> activePoolsQuery = queryFactory.select(
                 POOL_REQUEST.poolNumber,
                 POOL_REQUEST.totalNoRequired,
-                JUROR_POOL.isActive.count(),
+                activeJurorCountExpr,
                 POOL_REQUEST.courtLocation.name,
                 POOL_REQUEST.poolType.description,
                 POOL_REQUEST.returnDate
@@ -222,7 +229,7 @@ public class ActivePoolsRepositoryImpl implements IActivePoolsRepository {
             data -> PoolRequestActiveDataDto.builder()
                 .poolNumber(data.get(POOL_REQUEST.poolNumber))
                 .poolCapacity(data.get(POOL_REQUEST.totalNoRequired).intValue())
-                .jurorsInPool(data.get(JUROR_POOL.isActive.count()).intValue())
+                .jurorsInPool(data.get(activeJurorCountExpr).intValue())
                 .courtName(data.get(POOL_REQUEST.courtLocation.name))
                 .poolType(data.get(POOL_REQUEST.poolType.description))
                 .attendanceDate(data.get(POOL_REQUEST.returnDate))
