91dbbd2abdb919c9ddc6d162c22fec12c885d1d8
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2025-11-24 15:37:32 +0000

    JS-618 fix bug created by responded setting (#1033)

--- src/main/java/uk/gov/hmcts/juror/api/moj/domain/FormCode.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/domain/FormCode.java
@@ -4,39 +4,42 @@ import lombok.Getter;
 import uk.gov.hmcts.juror.api.moj.exception.MojException;
 import uk.gov.hmcts.juror.api.moj.service.PrintDataService;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 @Getter
 public enum FormCode {
 
-    ENG_EXCUSAL("5225", PrintDataService::printExcusalLetter, IJurorStatus.EXCUSED),
-    BI_EXCUSAL("5225C", PrintDataService::printExcusalLetter, IJurorStatus.EXCUSED),
-    ENG_EXCUSALDENIED("5226", PrintDataService::printExcusalDeniedLetter, IJurorStatus.SUMMONED),
-    BI_EXCUSALDENIED("5226C", PrintDataService::printExcusalDeniedLetter, IJurorStatus.SUMMONED),
-    ENG_SUMMONS("5221", PrintDataService::reprintSummonsLetter, IJurorStatus.SUMMONED),
-    BI_SUMMONS("5221C", PrintDataService::reprintSummonsLetter, IJurorStatus.SUMMONED),
-    ENG_SUMMONS_REMINDER("5228", PrintDataService::printSummonsReminderLetter, IJurorStatus.SUMMONED),
-    BI_SUMMONS_REMINDER("5228C", PrintDataService::printSummonsReminderLetter, IJurorStatus.SUMMONED),
-    ENG_CONFIRMATION("5224A", PrintDataService::printConfirmationLetter, IJurorStatus.RESPONDED),
-    BI_CONFIRMATION("5224AC", PrintDataService::printConfirmationLetter, IJurorStatus.RESPONDED),
-    ENG_DEFERRAL("5229A", PrintDataService::printDeferralLetter, IJurorStatus.DEFERRED),
-    BI_DEFERRAL("5229AC", PrintDataService::printDeferralLetter, IJurorStatus.DEFERRED),
-    ENG_DEFERRALDENIED("5226A", PrintDataService::printDeferralDeniedLetter, IJurorStatus.RESPONDED),
-    BI_DEFERRALDENIED("5226AC", PrintDataService::printDeferralDeniedLetter, IJurorStatus.RESPONDED),
-    ENG_POSTPONE("5229", PrintDataService::printPostponeLetter, IJurorStatus.DEFERRED),
-    BI_POSTPONE("5229C", PrintDataService::printPostponeLetter, IJurorStatus.DEFERRED),
+    ENG_EXCUSAL("5225", PrintDataService::printExcusalLetter, List.of(IJurorStatus.EXCUSED)),
+    BI_EXCUSAL("5225C", PrintDataService::printExcusalLetter, List.of(IJurorStatus.EXCUSED)),
+    ENG_EXCUSALDENIED("5226", PrintDataService::printExcusalDeniedLetter, List.of(IJurorStatus.SUMMONED,
+                                                                                  IJurorStatus.RESPONDED)),
+    BI_EXCUSALDENIED("5226C", PrintDataService::printExcusalDeniedLetter, List.of(IJurorStatus.SUMMONED,
+                                                                                  IJurorStatus.RESPONDED)),
+    ENG_SUMMONS("5221", PrintDataService::reprintSummonsLetter, List.of(IJurorStatus.SUMMONED)),
+    BI_SUMMONS("5221C", PrintDataService::reprintSummonsLetter, List.of(IJurorStatus.SUMMONED)),
+    ENG_SUMMONS_REMINDER("5228", PrintDataService::printSummonsReminderLetter, List.of(IJurorStatus.SUMMONED)),
+    BI_SUMMONS_REMINDER("5228C", PrintDataService::printSummonsReminderLetter, List.of(IJurorStatus.SUMMONED)),
+    ENG_CONFIRMATION("5224A", PrintDataService::printConfirmationLetter, List.of(IJurorStatus.RESPONDED)),
+    BI_CONFIRMATION("5224AC", PrintDataService::printConfirmationLetter, List.of(IJurorStatus.RESPONDED)),
+    ENG_DEFERRAL("5229A", PrintDataService::printDeferralLetter, List.of(IJurorStatus.DEFERRED)),
+    BI_DEFERRAL("5229AC", PrintDataService::printDeferralLetter, List.of(IJurorStatus.DEFERRED)),
+    ENG_DEFERRALDENIED("5226A", PrintDataService::printDeferralDeniedLetter, List.of(IJurorStatus.RESPONDED)),
+    BI_DEFERRALDENIED("5226AC", PrintDataService::printDeferralDeniedLetter, List.of(IJurorStatus.RESPONDED)),
+    ENG_POSTPONE("5229", PrintDataService::printPostponeLetter, List.of(IJurorStatus.DEFERRED)),
+    BI_POSTPONE("5229C", PrintDataService::printPostponeLetter, List.of(IJurorStatus.DEFERRED)),
     // The additional information status on juror is not used in modernisation code
-    ENG_REQUESTINFO("5227", PrintDataService::reprintRequestInfoLetter, IJurorStatus.SUMMONED),
-    BI_REQUESTINFO("5227C", PrintDataService::reprintRequestInfoLetter, IJurorStatus.SUMMONED),
-    ENG_WITHDRAWAL("5224", PrintDataService::printWithdrawalLetter, IJurorStatus.DISQUALIFIED),
-    BI_WITHDRAWAL("5224C", PrintDataService::printWithdrawalLetter, IJurorStatus.DISQUALIFIED);
+    ENG_REQUESTINFO("5227", PrintDataService::reprintRequestInfoLetter, List.of(IJurorStatus.SUMMONED)),
+    BI_REQUESTINFO("5227C", PrintDataService::reprintRequestInfoLetter, List.of(IJurorStatus.SUMMONED)),
+    ENG_WITHDRAWAL("5224", PrintDataService::printWithdrawalLetter, List.of(IJurorStatus.DISQUALIFIED)),
+    BI_WITHDRAWAL("5224C", PrintDataService::printWithdrawalLetter, List.of(IJurorStatus.DISQUALIFIED));
     private final String code;
 
-    private final int jurorStatus;
+    private final List<Integer> jurorStatus;
 
     private final BiConsumer<PrintDataService, JurorPool> letterPrinter;
 
-    FormCode(String code, BiConsumer<PrintDataService, JurorPool> letterPrinter, int jurorStatus) {
+    FormCode(String code, BiConsumer<PrintDataService, JurorPool> letterPrinter, List<Integer> jurorStatus) {
         this.code = code;
         this.letterPrinter = letterPrinter;
         this.jurorStatus = jurorStatus;
--- src/test/java/uk/gov/hmcts/juror/api/moj/enumeration/FormCodeTypeTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/enumeration/FormCodeTypeTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.juror.api.moj.domain.FormCode;
 import uk.gov.hmcts.juror.api.moj.domain.IJurorStatus;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SuppressWarnings({
@@ -14,95 +16,95 @@ class FormCodeTypeTest {
 
     @Test
     void englishExcusal() {
-        assertEquals(FormCode.ENG_EXCUSAL, "5225", IJurorStatus.EXCUSED);
+        assertEquals(FormCode.ENG_EXCUSAL, "5225", List.of(IJurorStatus.EXCUSED));
     }
 
     @Test
     void welshExcusal() {
-        assertEquals(FormCode.BI_EXCUSAL, "5225C", IJurorStatus.EXCUSED);
+        assertEquals(FormCode.BI_EXCUSAL, "5225C", List.of(IJurorStatus.EXCUSED));
     }
 
     @Test
     void englishExcusalDenied() {
-        assertEquals(FormCode.ENG_EXCUSALDENIED, "5226", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.ENG_EXCUSALDENIED, "5226", List.of(IJurorStatus.SUMMONED, IJurorStatus.RESPONDED));
     }
 
     @Test
     void welshExcusalDenied() {
-        assertEquals(FormCode.BI_EXCUSALDENIED, "5226C", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.BI_EXCUSALDENIED, "5226C", List.of(IJurorStatus.SUMMONED, IJurorStatus.RESPONDED));
     }
 
     @Test
     void englishSummons() {
-        assertEquals(FormCode.ENG_SUMMONS, "5221", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.ENG_SUMMONS, "5221", List.of(IJurorStatus.SUMMONED));
     }
 
     @Test
     void welshSummons() {
-        assertEquals(FormCode.BI_SUMMONS, "5221C", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.BI_SUMMONS, "5221C", List.of(IJurorStatus.SUMMONED));
     }
 
     @Test
     void englishConfirmation() {
-        assertEquals(FormCode.ENG_CONFIRMATION, "5224A", IJurorStatus.RESPONDED);
+        assertEquals(FormCode.ENG_CONFIRMATION, "5224A", List.of(IJurorStatus.RESPONDED));
     }
 
     @Test
     void welshConfirmation() {
-        assertEquals(FormCode.BI_CONFIRMATION, "5224AC", IJurorStatus.RESPONDED);
+        assertEquals(FormCode.BI_CONFIRMATION, "5224AC", List.of(IJurorStatus.RESPONDED));
     }
 
     @Test
     void englishDeferral() {
-        assertEquals(FormCode.ENG_DEFERRAL, "5229A", IJurorStatus.DEFERRED);
+        assertEquals(FormCode.ENG_DEFERRAL, "5229A", List.of(IJurorStatus.DEFERRED));
     }
 
     @Test
     void welshDeferral() {
-        assertEquals(FormCode.BI_DEFERRAL, "5229AC", IJurorStatus.DEFERRED);
+        assertEquals(FormCode.BI_DEFERRAL, "5229AC", List.of(IJurorStatus.DEFERRED));
     }
 
     @Test
     void englishDeferralDenied() {
-        assertEquals(FormCode.ENG_DEFERRALDENIED, "5226A", IJurorStatus.RESPONDED);
+        assertEquals(FormCode.ENG_DEFERRALDENIED, "5226A", List.of(IJurorStatus.RESPONDED));
     }
 
     @Test
     void welshDeferralDenied() {
-        assertEquals(FormCode.BI_DEFERRALDENIED, "5226AC", IJurorStatus.RESPONDED);
+        assertEquals(FormCode.BI_DEFERRALDENIED, "5226AC", List.of(IJurorStatus.RESPONDED));
     }
 
     @Test
     void englishPostpone() {
-        assertEquals(FormCode.ENG_POSTPONE, "5229", IJurorStatus.DEFERRED);
+        assertEquals(FormCode.ENG_POSTPONE, "5229", List.of(IJurorStatus.DEFERRED));
     }
 
     @Test
     void welshPostpone() {
-        assertEquals(FormCode.BI_POSTPONE, "5229C", IJurorStatus.DEFERRED);
+        assertEquals(FormCode.BI_POSTPONE, "5229C", List.of(IJurorStatus.DEFERRED));
     }
 
     @Test
     void englishRequestInfo() {
-        assertEquals(FormCode.ENG_REQUESTINFO, "5227", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.ENG_REQUESTINFO, "5227", List.of(IJurorStatus.SUMMONED));
     }
 
     @Test
     void welshRequestInfo() {
-        assertEquals(FormCode.BI_REQUESTINFO, "5227C", IJurorStatus.SUMMONED);
+        assertEquals(FormCode.BI_REQUESTINFO, "5227C", List.of(IJurorStatus.SUMMONED));
     }
 
     @Test
     void englishWithdrawal() {
-        assertEquals(FormCode.ENG_WITHDRAWAL, "5224", IJurorStatus.DISQUALIFIED);
+        assertEquals(FormCode.ENG_WITHDRAWAL, "5224", List.of(IJurorStatus.DISQUALIFIED));
     }
 
     @Test
     void welshWithdrawal() {
-        assertEquals(FormCode.BI_WITHDRAWAL, "5224C", IJurorStatus.DISQUALIFIED);
+        assertEquals(FormCode.BI_WITHDRAWAL, "5224C", List.of(IJurorStatus.DISQUALIFIED));
     }
 
-    private void assertEquals(FormCode type, String code, int jurorStatus) {
+    private void assertEquals(FormCode type, String code, List<Integer> jurorStatus) {
         assertThat(type.getCode())
             .isEqualTo(code);
         assertThat(type.getJurorStatus()).isEqualTo(jurorStatus);
