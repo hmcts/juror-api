1132b2594217f41cf27129cd3cef1693fb5a27b2
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2025-12-09 16:42:49 +0000

    update query for loc code

--- src/main/java/uk/gov/hmcts/juror/api/moj/report/standard/WeekendAttendanceReport.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/report/standard/WeekendAttendanceReport.java
@@ -50,7 +50,7 @@ public class WeekendAttendanceReport extends AbstractStandardReport {
     @Override
     protected void preProcessQuery(JPAQuery<Tuple> query, StandardReportRequest request) {
 
-        query.where(QAppearance.appearance.locCode.in(SecurityUtil.getCourts()));
+        query.where(QAppearance.appearance.locCode.in(request.getLocCode()));
 
         // need to work out the weekend and bank holiday dates in the range
         // get first day of current month
