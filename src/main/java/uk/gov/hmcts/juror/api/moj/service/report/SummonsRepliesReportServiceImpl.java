3159807a1efb06e2c6984ff60fb61c31d979768d
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2026-01-30 12:13:56 +0000

    Task/JS-724  I want a report to see how many digital responses have been completed (#1064)
    * task/JS-724 I want a report to see how many digital responses have been completed
    
    * updated core code and tests
    
    * added csv functionality
    
    * updates following review
    
    * updating integration test
    
    * updating tests
    
    * minor updates

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/report/SummonsRepliesReportServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/report/SummonsRepliesReportServiceImpl.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.juror.api.moj.service.report;
 
 import com.querydsl.core.Tuple;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.juror.api.moj.controller.reports.response.AbstractReportResponse;
 import uk.gov.hmcts.juror.api.moj.controller.reports.response.DigitalSummonsRepliesReportResponse;
+import uk.gov.hmcts.juror.api.moj.controller.reports.response.ResponsesCompletedReportResponse;
+import uk.gov.hmcts.juror.api.moj.exception.MojException;
 import uk.gov.hmcts.juror.api.moj.repository.jurorresponse.JurorDigitalResponseRepositoryModImpl;
+import uk.gov.hmcts.juror.api.moj.service.summonsmanagement.JurorResponseService;
+import uk.gov.hmcts.juror.api.moj.utils.SecurityUtil;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -17,12 +22,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SummonsRepliesReportServiceImpl implements SummonsRepliesReportService {
     private final JurorDigitalResponseRepositoryModImpl jurorDigitalResponseRepositoryMod;
+    private final JurorResponseService jurorResponseService;
 
 
     @Override
@@ -37,8 +45,8 @@ public class SummonsRepliesReportServiceImpl implements SummonsRepliesReportServ
             for (Tuple result : results) {
                 DigitalSummonsRepliesReportResponse.TableData.DataRow dataRow =
                     new DigitalSummonsRepliesReportResponse.TableData.DataRow();
-                dataRow.setDate(result.get(0, Date.class).toLocalDate());
-                final int total = result.get(1, Long.class).intValue();
+                dataRow.setDate(Objects.requireNonNull(result.get(0, Date.class)).toLocalDate());
+                final int total = Objects.requireNonNull(result.get(1, Long.class)).intValue();
                 dataRow.setNoOfReplies(total);
                 totalReplies += total;
                 dataRows.add(dataRow);
@@ -51,6 +59,134 @@ public class SummonsRepliesReportServiceImpl implements SummonsRepliesReportServ
         reportResponse.getTableData().setData(dataRows);
 
         return reportResponse;
+    }
+
+    @Override
+    public ResponsesCompletedReportResponse getResponsesCompletedReport(LocalDate monthStartDate) {
+        // log the method entry
+        log.info("Generating Responses Completed Report for month starting: {}", monthStartDate);
+
+        //check user is a bureau manager
+        if (!SecurityUtil.isBureauManager()) {
+            throw new MojException.Forbidden("User is not a bureau manager", null);
+        }
+
+        ResponsesCompletedReportResponse.TableData.Heading staffNameHeading =
+            new ResponsesCompletedReportResponse.TableData.Heading();
+        staffNameHeading.setId(0);
+        staffNameHeading.setName("Staff Name");
+        staffNameHeading.setDataType(String.class.getSimpleName());
+
+        // need a count of the total responses completed on each day
+        Map<LocalDate, Integer> totalResponsesByDate = new ConcurrentHashMap<>();
+        final List<SummonsRepliesReportService.CompletedResponseRecord> completedResponses =
+            jurorResponseService.getResponsesCompletedReport(monthStartDate);
+
+        // for each staff member, build a data row
+        final List<ResponsesCompletedReportResponse.TableData.DataRow> dataRows = getDataRows(
+            monthStartDate,
+            completedResponses,
+            totalResponsesByDate
+        );
+
+        // add a row for total responses per day
+        ResponsesCompletedReportResponse.TableData.DataRow totalRow =
+            new ResponsesCompletedReportResponse.TableData.DataRow();
+        totalRow.setStaffName("Total Responses");
+        List<Integer> totalDailyTotals = new ArrayList<>();
+        int grandTotal = 0;
+        LocalDate tempDate = monthStartDate;
+        while (tempDate.getMonth() == monthStartDate.getMonth()) {
+            Integer dailyTotal = totalResponsesByDate.getOrDefault(tempDate, 0);
+            totalDailyTotals.add(dailyTotal);
+            grandTotal += dailyTotal;
+            tempDate = tempDate.plusDays(1);
+        }
+        totalRow.setDailyTotals(totalDailyTotals);
+        totalRow.setStaffTotal(grandTotal);
+
+        dataRows.add(totalRow);
+
+        ResponsesCompletedReportResponse reportResponse = new ResponsesCompletedReportResponse(
+            getResponsesCompletedReportHeaders(grandTotal));
+
+        List<ResponsesCompletedReportResponse.TableData.Heading> tableHeadings = getTableHeadings(
+            monthStartDate,
+            staffNameHeading
+        );
+
+        reportResponse.getTableData().setHeadings(tableHeadings);
+        reportResponse.getTableData().setData(dataRows);
+
+        log.info("Completed Responses Completed Report generation for month starting: {}", monthStartDate);
+        return reportResponse;
+
+    }
+
+    private List<ResponsesCompletedReportResponse.TableData.DataRow> getDataRows(LocalDate monthStartDate,
+                 List<CompletedResponseRecord> completedResponses, Map<LocalDate, Integer> totalResponsesByDate) {
+        List<ResponsesCompletedReportResponse.TableData.DataRow> dataRows = new ArrayList<>();
+        for (CompletedResponseRecord completedResponseRecord : completedResponses) {
+
+            // the same staff member may have multiple records for different days, so check if we already have a row
+            ResponsesCompletedReportResponse.TableData.DataRow dataRow = dataRows.stream()
+                .filter(row -> row.getStaffName().equals(completedResponseRecord.staffName()))
+                .findFirst()
+                .orElse(null);
+            if (dataRow == null) {
+                // initialize the daily totals list with zeros for each day in the month
+                List<Integer> dailyTotals = new ArrayList<>();
+                LocalDate tempDate = monthStartDate;
+                while (tempDate.getMonth() == monthStartDate.getMonth()) {
+                    dailyTotals.add(0);
+                    tempDate = tempDate.plusDays(1);
+
+                }
+                dataRow = ResponsesCompletedReportResponse.TableData.DataRow.of(
+                    completedResponseRecord.staffName(),
+                    dailyTotals,
+                    0
+                );
+                dataRows.add(dataRow);
+            }
+            // set the completed responses for the appropriate day
+            int dayOfMonth = completedResponseRecord.date().getDayOfMonth();
+            dataRow.getDailyTotals().set(dayOfMonth - 1, completedResponseRecord.completedResponses());
+            // update the staff total
+            dataRow.setStaffTotal(dataRow.getStaffTotal() + completedResponseRecord.completedResponses());
+            // update the total responses by date
+            totalResponsesByDate.merge(completedResponseRecord.date(),
+                                       completedResponseRecord.completedResponses(),
+                                       Integer::sum);
+        }
+        return dataRows;
+    }
+
+    private List<ResponsesCompletedReportResponse.TableData.Heading> getTableHeadings(LocalDate monthStartDate,
+                                              ResponsesCompletedReportResponse.TableData.Heading staffNameHeading) {
+        List<ResponsesCompletedReportResponse.TableData.Heading> tableHeadings = new ArrayList<>();
+        tableHeadings.add(staffNameHeading);
+        // need to add a heading for each day in the month
+        int counter;
+        int daysInMonth = monthStartDate.lengthOfMonth();
+        for (counter = 1; counter <= daysInMonth; counter++) {
+            long day = counter - 1L;
+            tableHeadings.add(
+                ResponsesCompletedReportResponse.TableData.Heading.of(
+                    counter,
+                    monthStartDate.plusDays(day).toString(),
+                    Integer.class.getSimpleName()
+                )
+            );
+        }
+        // finally add a total heading
+        ResponsesCompletedReportResponse.TableData.Heading totalHeading =
+            new ResponsesCompletedReportResponse.TableData.Heading();
+        totalHeading.setId(counter); // counter is now at total position
+        totalHeading.setName("Total");
+        totalHeading.setDataType(Integer.class.getSimpleName());
+        tableHeadings.add(totalHeading);
+        return tableHeadings;
     }
 
 
@@ -76,10 +212,34 @@ public class SummonsRepliesReportServiceImpl implements SummonsRepliesReportServ
     }
 
 
+    private Map<String, AbstractReportResponse.DataTypeValue>
+        getResponsesCompletedReportHeaders(int totalProcessed) {
+        return Map.of(
+            "report_created", AbstractReportResponse.DataTypeValue.builder()
+                .displayName(ReportHeading.REPORT_CREATED.getDisplayName())
+                .dataType(ReportHeading.REPORT_CREATED.getDataType())
+                .value(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE))
+                .build(),
+            "time_created", AbstractReportResponse.DataTypeValue.builder()
+                .displayName(ReportHeading.TIME_CREATED.getDisplayName())
+                .dataType(ReportHeading.TIME_CREATED.getDataType())
+                .value(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .build(),
+            "responses_processed", AbstractReportResponse.DataTypeValue.builder()
+                .displayName(ReportHeading.RESPONSES_PROCESSED.getDisplayName())
+                .dataType(ReportHeading.RESPONSES_PROCESSED.getDataType())
+                .value(totalProcessed)
+                .build()
+        );
+    }
+
+
+    @Getter
     public enum ReportHeading {
         REPORT_CREATED("Report created", LocalDate.class.getSimpleName()),
         TIME_CREATED("Time created", LocalDateTime.class.getSimpleName()),
-        REPLY_COUNT("Total number of replies received", Integer.class.getSimpleName());
+        REPLY_COUNT("Total number of replies received", Integer.class.getSimpleName()),
+        RESPONSES_PROCESSED("Number of responses processed", Integer.class.getSimpleName());
 
         private final String displayName;
 
@@ -90,12 +250,5 @@ public class SummonsRepliesReportServiceImpl implements SummonsRepliesReportServ
             this.dataType = dataType;
         }
 
-        public String getDisplayName() {
-            return displayName;
-        }
-
-        public String getDataType() {
-            return dataType;
-        }
     }
 }
