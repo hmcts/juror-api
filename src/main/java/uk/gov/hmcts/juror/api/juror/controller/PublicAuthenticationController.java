33c64793833c2b99adb7dd51a7246e0f4f2df000
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2026-03-26 16:45:55 +0000

    JS-820 Adminstartion Management Update naming convention (#1102)

--- src/main/java/uk/gov/hmcts/juror/api/juror/controller/PublicAuthenticationController.java
+++ src/main/java/uk/gov/hmcts/juror/api/juror/controller/PublicAuthenticationController.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.juror.api.juror.controller;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -67,6 +69,7 @@ public class PublicAuthenticationController {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     @Data
     @Schema(description = "Public Juror login credentials")
     public static class PublicAuthenticationRequestDto implements Serializable {
