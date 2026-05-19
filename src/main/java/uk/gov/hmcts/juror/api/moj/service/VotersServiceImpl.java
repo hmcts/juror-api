71450d1401af09710321fd1dd18cd81ffa0d19c6
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2026-05-19 09:49:23 +0100

    Task/JS-620 avoid selecting voters that match details of jurors known to be deceased (#1139)
    * task/JS-620 Deceased voters
    
    * Update CreatePoolController_createPool.sql
    
    * updating the way deceased voters are retrieved
    
    * optimising using a custom class
    
    * Update VotersServiceImpl.java
    
    * fixing merge conflicts

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/VotersServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/VotersServiceImpl.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.juror.api.bureau.domain.SystemParameter;
 import uk.gov.hmcts.juror.api.bureau.domain.SystemParameterRepository;
+import uk.gov.hmcts.juror.api.juror.service.JurorService;
 import uk.gov.hmcts.juror.api.moj.controller.request.PoolCreateRequestDto;
+import uk.gov.hmcts.juror.api.moj.domain.DeceasedJuror;
 import uk.gov.hmcts.juror.api.moj.domain.ExcludedVoters;
 import uk.gov.hmcts.juror.api.moj.domain.QExcludedVoters;
 import uk.gov.hmcts.juror.api.moj.domain.QVoters;
@@ -29,6 +31,9 @@ import java.util.Optional;
 public class VotersServiceImpl implements VotersService {
     static final int AGE_LOWER_SP_ID = 101;
     static final int AGE_UPPER_SP_ID = 100;
+
+    @Autowired
+    private JurorService jurorService;
 
     private final VotersRepository votersRepository;
     private final SystemParameterRepository systemParameterRepository;
@@ -71,21 +76,38 @@ public class VotersServiceImpl implements VotersService {
         if (isCoroners) {
             query.where(QVoters.voters.flags.isNull());
         }
-        List<Voters> votersSelected = query.orderBy(NumberExpression.random().asc())
+        List<Voters> votersList = query.orderBy(NumberExpression.random().asc())
             .limit((long) (citizensToSummon * 1.4))
             .fetch();
 
         // need to do some filtering in case excluded or deceased voters were fetched
+
+        // create a list of all distinct postcodes from votersList
+        List<String> voterPostcodes = votersList.stream()
+            .map(Voters::getPostcode)
+            .distinct()
+            .toList();
+
+        List<DeceasedJuror> deceasedJurors = jurorService.getDeceasedJurors(voterPostcodes);
+        
+        // Filter out deceased voters
+        votersList.removeIf(voter -> deceasedJurors.stream()
+            .anyMatch(juror -> juror.getAddressLine1().trim().equalsIgnoreCase(voter.getAddress().trim())
+                && juror.getLastName().trim().equalsIgnoreCase(voter.getLastName().trim())
+                && juror.getFirstName().trim().equalsIgnoreCase(voter.getFirstName().trim())
+                && juror.getPostcode().trim().equalsIgnoreCase(voter.getPostcode().trim())));
+
+    
         List<ExcludedVoters> excludedVoters = getExcludedVoters();
 
         // filter out excluded voters
-        votersSelected.removeIf(voter -> excludedVoters.stream()
+        votersList.removeIf(voter -> excludedVoters.stream()
             .anyMatch(excludedVoter -> excludedVoter.getAddress1().trim().equalsIgnoreCase(voter.getAddress().trim())
                 && excludedVoter.getFirstName().trim().equalsIgnoreCase(voter.getFirstName().trim())
                 && excludedVoter.getLastName().trim().equalsIgnoreCase(voter.getLastName().trim())
                 && excludedVoter.getPostcode().trim().equalsIgnoreCase(voter.getPostcode().trim())));
 
-        return votersSelected;
+        return votersList;
     }
 
     @Override
