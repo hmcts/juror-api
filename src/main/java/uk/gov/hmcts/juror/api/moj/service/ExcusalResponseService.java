ae74a72cffc446ed10b363daf7152843ae687f55
tomhoughcgi <tom.hough@cgi.com>
2026-06-01 13:21:30 +0100

    Annotating these interfaces with @FunctionalInterface

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/ExcusalResponseService.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/ExcusalResponseService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.juror.api.moj.service;
 import uk.gov.hmcts.juror.api.config.bureau.BureauJwtPayload;
 import uk.gov.hmcts.juror.api.moj.controller.request.ExcusalDecisionDto;
 
+@FunctionalInterface
 public interface ExcusalResponseService {
 
     void respondToExcusalRequest(BureauJwtPayload payload, ExcusalDecisionDto excusalDecisionDto, String jurorNumber);
