388a93336c52ac31a7f1e30b688b9e24a28908e0
leano.samwinga <leano.samwinga@cgi.com>
2026-06-03 12:31:11 +0100

    StaffServiceImpl.java: Added relevant suppression(s), removed variable reassign

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/staff/StaffServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/staff/StaffServiceImpl.java
@@ -48,9 +48,7 @@ public class StaffServiceImpl implements StaffService {
 
     @Override
     @Transactional
-    @SuppressWarnings({"PMD.NcssCount",
-        "PMD.CognitiveComplexity",
-        "PMD.NPathComplexity"})
+    @SuppressWarnings({"PMD.NcssCount", "PMD.CognitiveComplexity", "PMD.NPathComplexity", "PMD.ImplicitFunctionalInterface"})
     public StaffAssignmentResponseDto changeAssignment(final StaffAssignmentRequestDto staffAssignmentRequestDto,
                                                        final String currentUser) {
         log.trace("enter changeAssignment");
@@ -170,7 +168,7 @@ public class StaffServiceImpl implements StaffService {
                 "Cannot change assignment to user " + JurorDigitalApplication.AUTO_USER, null);
         }
 
-        User assignToUser = null;
+        User assignToUser;
 
         log.trace("Retrieve user record from the database for username {}", username);
         assignToUser = userRepository.findByUsername(username);
