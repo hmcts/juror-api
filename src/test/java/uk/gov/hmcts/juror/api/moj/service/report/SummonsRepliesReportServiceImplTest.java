7660fc3c661b88b72144eeff4b44214d4051c40c
Andrew-Hall-cgi <an.hall@cgi.com>
2026-06-04 13:50:30 +0100

    SummonsRepliesReportServiceImplTest.java PMDs done.

--- src/test/java/uk/gov/hmcts/juror/api/moj/service/report/SummonsRepliesReportServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/service/report/SummonsRepliesReportServiceImplTest.java
@@ -41,7 +41,7 @@ class SummonsRepliesReportServiceImplTest {
     private final SummonsRepliesReportService summonsRepliesReportService;
     private final JurorResponseService jurorResponseService;
 
-    public SummonsRepliesReportServiceImplTest() {
+    private SummonsRepliesReportServiceImplTest() {
         this.jurorDigitalResponseRepositoryMod = mock(JurorDigitalResponseRepositoryModImpl.class);
         this.jurorResponseService = mock(JurorResponseService.class);
         this.summonsRepliesReportService = new SummonsRepliesReportServiceImpl(jurorDigitalResponseRepositoryMod,
@@ -111,7 +111,7 @@ class SummonsRepliesReportServiceImplTest {
             assertThat(headings.get("reply_count")).isEqualTo(AbstractReportResponse.DataTypeValue.builder()
                 .displayName("Total number of replies received")
                 .dataType("Integer")
-                .value(Integer.valueOf(0))
+                .value(0)
                 .build());
 
             AbstractReportResponse.DataTypeValue timeCreated = headings.get("time_created");
