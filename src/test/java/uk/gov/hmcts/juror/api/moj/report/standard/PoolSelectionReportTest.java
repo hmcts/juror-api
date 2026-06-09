3342d59bba63ae40fc22999bbeb3a89f378a1559
Andrew-Hall-cgi <an.hall@cgi.com>
2026-06-09 12:22:46 +0100

    PoolSelectionReportTest.java PMDs done.

--- src/test/java/uk/gov/hmcts/juror/api/moj/report/standard/PoolSelectionReportTest.java
+++ src/test/java/uk/gov/hmcts/juror/api/moj/report/standard/PoolSelectionReportTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 class PoolSelectionReportTest extends AbstractStandardReportTestSupport<PoolSelectionListReport> {
-    public PoolSelectionReportTest() {
+    PoolSelectionReportTest() {
         super(QJurorPool.jurorPool,
             PoolSelectionListReport.RequestValidator.class,
             DataType.JUROR_NUMBER,
