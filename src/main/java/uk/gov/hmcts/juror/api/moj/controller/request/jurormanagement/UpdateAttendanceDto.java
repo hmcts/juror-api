c622ee9d263354b6ad87766adbc567dca0e479ff
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2026-06-19 08:37:07 +0100

    JS-823 UpdateAttendanceDto api naming standards

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/jurormanagement/UpdateAttendanceDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/jurormanagement/UpdateAttendanceDto.java
@@ -29,21 +29,17 @@ import java.util.List;
 @Builder
 @Schema(description = "Update attendance (appearance) details")
 public class UpdateAttendanceDto {
-    @JsonProperty("commonData")
     @Valid
     @Schema(description = "Common data relating to updating attendance details")
     private CommonData commonData;
 
-    @JsonProperty("juror")
     @Schema(description = "List of (one or more) jurors to update attendance record of")
     private List<String> juror;
 
-    @JsonProperty("trial_number")
     @Schema(description = "Trial number of the jurors that attendance is being updated for, where applicable")
     @Length(max = 16)
     private String trialNumber;
 
-    @JsonProperty("juror_in_waiting")
     private boolean jurorInWaiting;
 
     @AllArgsConstructor
@@ -52,7 +48,7 @@ public class UpdateAttendanceDto {
     @Setter
     @Schema(description = "Common data relating to updating attendance details")
     public static class CommonData {
-        @JsonProperty("status")
+
         @NotNull(message = "status is mandatory")
         @Schema(description = "Enum values for attendance status, required for updating attendance details, e.g."
             + "CHECK_IN, "
@@ -60,27 +56,22 @@ public class UpdateAttendanceDto {
             + "CONFIRM_ATTENDANCE")
         private UpdateAttendanceStatus status;
 
-        @JsonProperty("attendanceDate")
         @NotNull(message = "attendanceDate is mandatory")
         @Schema(description = "Attendance date for jury service")
         @JsonFormat(pattern = "yyyy-MM-dd")
         private LocalDate attendanceDate;
 
-        @JsonProperty("locationCode")
         @Size(min = 3, max = 3)
         @NumericString
         @Schema(description = "Unique 3 digit code to identify a court location")
         private String locationCode;
 
-        @JsonProperty("checkInTime")
         @Schema(description = "Check in time of the juror", implementation = String.class, pattern = "HH24:mm")
         private LocalTime checkInTime;
 
-        @JsonProperty("checkOutTime")
         @Schema(description = "Check out time of the juror", implementation = String.class, pattern = "HH24:mm")
         private LocalTime checkOutTime;
 
-        @JsonProperty("singleJuror")
         @Schema(description = "Boolean flag to indicate if a single juror is to be updated")
         @NotNull
         private Boolean singleJuror;
