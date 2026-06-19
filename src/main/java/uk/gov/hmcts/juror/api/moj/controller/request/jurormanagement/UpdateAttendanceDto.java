2a92773b277d3eb6985c2f3e80080f256d00a225
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2026-06-19 09:05:59 +0100

    JS-823 UpdateAttendanceDto api naming standards

--- src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/jurormanagement/UpdateAttendanceDto.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/controller/request/jurormanagement/UpdateAttendanceDto.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.juror.api.moj.controller.request.jurormanagement;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
