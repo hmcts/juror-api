d956b5ba1698833a2c63434d4bf89ad326098df0
EPatterson1 <140503822+EPatterson1@users.noreply.github.com>
2026-08-21 16:54:36 +0100

    JS-814 amend mapper for thirdparty

--- src/main/java/uk/gov/hmcts/juror/api/moj/domain/NotifyTemplateMapperMod.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/domain/NotifyTemplateMapperMod.java
@@ -36,7 +36,7 @@ public enum NotifyTemplateMapperMod {
         Type.JUROR,
             context -> {
                 String jurorEmail = context.getJuror().getEmail();
-                if (jurorEmail != null && !jurorEmail.trim().isEmpty()) {
+                if (jurorEmail != null && !jurorEmail.isBlank()) {
                     return jurorEmail;
                 }
 
@@ -44,7 +44,12 @@ public enum NotifyTemplateMapperMod {
                     return null;
                 }
 
-                return context.getAbstractResponse().getEmail();
+                String responseEmail = context.getAbstractResponse().getEmail();
+                if (responseEmail != null && !responseEmail.isBlank()) {
+                    return responseEmail;
+                }
+
+                return context.getAbstractResponse().getEmailAddress();
             }
     ),
 
