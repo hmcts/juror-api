1059d6b93ea6062d245ec7b1e8298ee1c44bec9b
akikrahman1 <149579836+akikrahman1@users.noreply.github.com>
2026-09-17 09:58:52 +0100

    task/JS-1070 Juror count incorrect when court add or subtract jurors from a pool

--- src/main/java/uk/gov/hmcts/juror/api/moj/service/PoolStatisticsServiceImpl.java
+++ src/main/java/uk/gov/hmcts/juror/api/moj/service/PoolStatisticsServiceImpl.java
@@ -40,7 +40,8 @@ public class PoolStatisticsServiceImpl implements PoolStatisticsService {
 
         PoolSummaryResponseDto poolSummaryResponse = new PoolSummaryResponseDto();
         populatePoolDetailsData(poolSummaryResponse, poolRequest);
-        populateBureauSummoningData(poolSummaryResponse, poolStatistics, poolRequest.getNumberRequested());
+        final int noRequestedFromBureau = poolRequest.getTotalNoRequired() - poolStatistics.getCourtSupply();
+        populateBureauSummoningData(poolSummaryResponse, poolStatistics, noRequestedFromBureau);
         populatePoolSummaryData(poolSummaryResponse, poolStatistics, poolRequest.getTotalNoRequired());
         populateAdditionalStatsData(poolSummaryResponse, poolStatistics, poolRequest);
 
